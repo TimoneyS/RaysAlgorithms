8e572c0b7267e4940b06c509cc603da7f798c249
rays1 <rays1@LAPTOP-D217L5V1>
2017-06-08 12:14:25 +0800

    Signed-off-by: rays1 <rays1@LAPTOP-D217L5V1>

--- RaysAlgorithms/src/com/ray/astar/CellType.java
+++
@@ -1,5 +0,0 @@
-package com.ray.astar;
-
-public enum CellType { 
-	UNCHECK, CLOSE, OPEN, CHOOSE, BLOCK
-}
--- RaysAlgorithms/src/com/ray/search/SymbolTable.java
+++
@@ -1,6 +0,0 @@
-package com.ray.search;
-
-public interface SymbolTable<Key extends Comparable<Key>, Value> {
-	public void put(Key key, Value value);
-	public Value get(Key key);
-}

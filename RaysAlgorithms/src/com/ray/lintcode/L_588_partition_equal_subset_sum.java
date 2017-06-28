c1e470cde0c5ad5700d19f02adc08c8ba3f43222
rays1 <rays1@Rays-PC>
2017-06-28 17:26:19 +0800

    Signed-off-by: rays1 <rays1@Rays-PC>

--- RaysAlgorithms/src/com/ray/lintcode/L_588_partition_equal_subset_sum.java
+++ RaysAlgorithms/src/com/ray/lintcode/L_588_partition_equal_subset_sum.java
@@ -15,7 +15,40 @@ import com.ray.utils.StdOut;
  */
 public class L_588_partition_equal_subset_sum {
 	
-	static Solution Solution = new Solution();
+	static Solution2 Solution = new Solution2();
+	
+	static class Solution2 {
+		/**
+	     * @param nums a non-empty array only positive integers
+	     * @return return true if can partition or false
+	     */
+	   public boolean canPartition(int[] nums) {
+	    	int sum = 0;
+	    	for (int i : nums) sum += i;
+	    	if (sum%2!=0) return false;
+	    	return find(nums, 0, nums.length - 1, sum / 2);
+	    }
+	    
+	    public boolean find(int[] nums, int start, int end, int sum) {
+	    	if (sum == 0 || sum == nums[start]) return true;
+	    	else if (start >= end) return false;
+	    	boolean rs1 = false, rs2 = false;
+	    	
+	    	if (nums[start] < sum)
+	    		rs1 = find(nums, start + 1, end, sum - nums[start]); // 包含当前元素
+	    	if (!rs1) rs2 = find(nums, start + 1, end, sum);				  // 不包含当前元素
+	    	return rs1 || rs2;
+	    }
+	    
+	    public int sum(int[] nums, int[] trace) {
+	    	int sum = 0;
+	    	for (int i = 0; i < nums.length; i++) {
+	    		if (trace[i] == 1){ sum += nums[i];}
+	    	}
+	    	return sum;
+	    }
+	    
+	}
 	
 	static class Solution {
 	    /**
@@ -64,7 +97,7 @@ public class L_588_partition_equal_subset_sum {
 	
 	public static void main(String[] args) {
 		//test(new int[]{1, 5, 11, 5},  true); // two subsets: [1, 5, 5], [11]
-		//test(new int[]{1, 2,  3, 9}, false);
+		test(new int[]{1, 2,  3, 9}, false);
 		//test(new int[]{1,4,5,6,1,2,4,1,3,4,1,2,4,5,1,91,4,5,6,1,2,4,1,3,4,1,2,4,5,1}, false);
 		//test(new int[]{100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100}, false);
 	}

a47fa11e138fedb3ee04d770490343c1fa4fddc8
rays1 <rays1@Rays-PC>
2017-06-28 10:08:19 +0800

    add test method
    Signed-off-by: rays1 <rays1@Rays-PC>

--- RaysAlgorithms/src/com/ray/utils/TestUnit.java
+++ RaysAlgorithms/src/com/ray/utils/TestUnit.java
@@ -7,5 +7,14 @@ public class TestUnit {
 		else 			StdOut.err(String.format("`%s` not equals `%s`", a, b));
 	}
 	
+	public static void assertTrue(boolean result) {
+		if(result) StdOut.p("result is true, expect true");
+		else 	   StdOut.err("reult is false, expect true");
+	}
+	
+	public static void assertFalse(boolean result) {
+		if(result) StdOut.err("result is true, expect false");
+		else 	   StdOut.p(  "result is false, expect false");
+	}
 	
 }

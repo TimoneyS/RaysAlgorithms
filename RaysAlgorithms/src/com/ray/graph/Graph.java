c9f62d43f23ed08d05fc4967c619114751b312c3
rays1 <rays1@Rays-PC>
2017-07-03 10:14:12 +0800

    增加表示图的类
    Signed-off-by: rays1 <rays1@Rays-PC>

---
+++ RaysAlgorithms/src/com/ray/graph/Graph.java
@@ -0,0 +1,20 @@
+package com.ray.graph;
+
+import java.io.InputStream;
+
+public class Graph {
+	
+	public Graph(int V) {
+	}
+	public Graph(InputStream in) {
+		
+	}
+	int V() { return 0; }
+	int E() { return 0; }
+	void addEdge(int v, int w) {
+		
+	}
+	Iterable<Integer> adj(int v) { return null; }
+	public String toString() {return super.toString();}
+	
+}

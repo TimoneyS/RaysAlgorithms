6811829414c3c47dcd554b649b4ed95328181c3e
rays1 <rays1@Rays-PC>
2017-11-13 19:54:50 +0800

    加权图的包和API

---
+++ RaysAlgorithms/src/com/ray/graph/weightedGraph/TestGraphEW.java
@@ -0,0 +1,26 @@
+package com.ray.graph.weightedGraph;
+
+import java.util.Scanner;
+
+import com.ray.utils.In;
+import com.ray.utils.Out;
+
+/**
+ * ≤‚ ‘ŒﬁœÚÕº
+ * @author rays1
+ *
+ */
+public class TestGraphEW {
+    
+    public static void main(String[] args) {
+        getDigraph("graphEW.txt");
+    }
+    
+    public static EdgeWeightedGraph getDigraph(String fileName) {
+        Scanner in = In.getProjectScanner("src/com/ray/graph/weightedGraph/" + fileName);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        Out.p(G);
+        return G;
+    }
+    
+}

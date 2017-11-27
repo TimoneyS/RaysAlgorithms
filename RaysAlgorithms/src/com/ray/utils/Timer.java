6e07eb7ff8d5b9382928fc9a3b63c6bea9ad8dd5
rays1 <rays1@Rays-PC>
2017-11-27 10:47:34 +0800

    新增方法

--- RaysAlgorithms/src/com/ray/utils/Timer.java
+++ RaysAlgorithms/src/com/ray/utils/Timer.java
@@ -2,6 +2,7 @@ package com.ray.utils;
 
 import static com.ray.utils.Out.*;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -13,6 +14,8 @@ public class Timer {
 	
 	private static Date RECORD_DATE = null;// = new Date();
 	private static Integer RECORD_COUNT = 1;
+    private static SimpleDateFormat dateFormater = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss:ms]"); 
+	
 	
 	/**
 	 * “点击” 计时器， 第一次点击 开始记录时间，之后每次按下将显示前一次的计时并开始下一次计时
@@ -36,6 +39,14 @@ public class Timer {
 		pf("第%2d次耗时记录：%s ms. \n", RECORD_COUNT ++, new Date().getTime() - RECORD_DATE.getTime());
 		RECORD_DATE  = null;
 		RECORD_COUNT = 1;
+	}
+	
+	/**
+	 * 时间戳
+	 */
+	public static String tag() {
+        return dateFormater.format(new Date());
+	    
 	}
 	
 }

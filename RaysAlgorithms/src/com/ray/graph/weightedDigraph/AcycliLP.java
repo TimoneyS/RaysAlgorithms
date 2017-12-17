ba437fee435ea20670f2552d9551e5cc43289260
rays1 <rays1@Rays-PC>
2017-12-17 16:42:24 +0800

    无环加权有向图，最长路径搜索

---
+++ RaysAlgorithms/src/com/ray/graph/weightedDigraph/AcycliLP.java
@@ -0,0 +1,38 @@
+package com.ray.graph.weightedDigraph;
+
+import java.util.Stack;
+
+import com.ray.utils.collections.RaysStack;
+
+/**
+ * 无环加权有向图最长路径搜索方法<br>
+ * 复制一张新的图，将图的所有路径的权设为原来的负值，然后对其进行最短路径搜索。
+ * @author rays1
+ * 
+ */
+public class AcycliLP {
+    
+    AcycliSP acycliSP;
+    
+    public AcycliLP(EdgeWeightedDigraph G, int S) {
+        EdgeWeightedDigraph G_R = G.copy();
+        
+        for (DirectedEdge e : G_R.edges() ) {
+            e.weighted();
+        }
+
+    }
+    
+    double distTo(int v) {
+        return 1;
+    }
+
+    boolean hasPathTo(int v) {
+        return false;
+    }
+    
+    RaysStack<DirectedEdge> pathTo(int v) {
+        return null;
+    }
+    
+}

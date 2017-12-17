9da7e66a7d07dfb4acb03297bfb146c540d1664a
rays1 <rays1@Rays-PC>
2017-12-17 16:47:18 +0800

    Dijkstra算法

--- RaysAlgorithms/src/com/ray/graph/weightedDigraph/DijkstraSP.java
+++ RaysAlgorithms/src/com/ray/graph/weightedDigraph/DijkstraSP.java
@@ -67,35 +67,4 @@ public class DijkstraSP {
         return path;
     }
 
-    /**
-     * 边的松弛
-     * 
-     * @param e
-     */
-    @SuppressWarnings("unused")
-    private void realx(DirectedEdge e) {
-        int v = e.from(), w = e.to();
-        if (distTo[w] > distTo[v] + e.weighted()) {
-            distTo[w] = distTo[v] + e.weighted();
-            edgeTo[w] = e;
-        }
-    }
-
-    /**
-     * 松弛顶点 （即松弛和顶点连接的所有的边）
-     * 
-     * @param G
-     * @param v
-     */
-    @SuppressWarnings("unused")
-    private void relax(EdgeWeightedDigraph G, int v) {
-        for (DirectedEdge e : G.adj(v)) {
-            int w = e.to();
-            if (distTo[w] > distTo[v] + e.weighted()) {
-                distTo[w] = distTo[v] + e.weighted();
-                edgeTo[w] = e;
-            }
-        }
-    }
-
 }

f7517f774a3d702426acb11a99edaad8aa84cfb2
rays1 <rays1@Rays-PC>
2017-12-25 00:06:07 +0800

    无环有向图，最长路径

--- RaysAlgorithms/src/com/ray/graph/gwd/AcycliLP.java
+++ RaysAlgorithms/src/com/ray/graph/gwd/AcycliLP.java
@@ -3,19 +3,15 @@ package com.ray.graph.gwd;
 import java.util.Stack;
 
 import com.ray.graph.gd.TopoLogical;
-import com.ray.utils.Out;
-import com.ray.utils.collections.RaysStack;
 
 /**
  * 无环加权有向图最长路径搜索方法<br>
- * 复制一张新的图，将图的所有路径的权设为原来的负值，然后对其进行最短路径搜索。
+ * 最长路径和最短路径搜索思想一致。<br>
+ * 不同之处在于，初始化时边的距离为负无穷，在放松时取更远的边
  * @author rays1
  * 
  */
-public class AcycliLP {
-    
-    private DirectedEdge[]      edgeTo;
-    private double[]            distTo;
+public class AcycliLP extends SP {
     
     public AcycliLP(EdgeWeightedDigraph G, int S) {
         
@@ -29,19 +25,17 @@ public class AcycliLP {
         
         TopoLogical topo = new TopoLogical(G);
         Stack<Integer> stack = topo.order();
-Out.p(stack);
         while (!stack.empty()) {
             for (DirectedEdge e : G.adj(stack.pop()))
                 relax(e);
         }
-Out.p("end");       
     }
     
     /**
      * 放松边，和最短路径的放松相反，此处是取较长的边
      * @param e
      */
-    private void relax(DirectedEdge e) {
+    protected void relax(DirectedEdge e) {
         int v = e.from(), w = e.to();                       // 边的起点和终点
         if (distTo[w] < distTo[v] + e.weighted()) {         // 从该边起点到 w 的距离，比从生成树到 w 要远
             edgeTo[w] = e;                                  // 将到 w 的边设置为该边
@@ -49,23 +43,8 @@ Out.p("end");
         }
     }
 
-    double distTo(int v) {
-        return distTo[v];
-    }
-
-    boolean hasPathTo(int v) {
+    public boolean hasPathTo(int v) {
         return distTo[v] > Double.NEGATIVE_INFINITY;
-    }
-    
-    RaysStack<DirectedEdge> pathTo(int v) {
-        if (!hasPathTo(v))
-            return null;
-
-        RaysStack<DirectedEdge> path = new RaysStack<DirectedEdge>();
-        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()])
-            path.push(e);
-
-        return path;
     }
     
 }

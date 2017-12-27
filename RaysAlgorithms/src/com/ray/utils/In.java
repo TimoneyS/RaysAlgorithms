883936dd31e1b8e5d1b0d9d621378d82351ebd12
rays1 <rays1@192.168.32.71>
2017-12-27 16:19:38 +0800

    输入流 增加指定包的方法

--- RaysAlgorithms/src/com/ray/utils/In.java
+++ RaysAlgorithms/src/com/ray/utils/In.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class In {
     
     /**
-     * 
+     * 从工程目录获取Scanner
      * @param res
      * @return
      */
@@ -21,6 +21,11 @@ public class In {
         return in;
     }
     
+    /**
+     * 从类路径获取资源的Scanner
+     * @param res
+     * @return
+     */
     public static Scanner getClassPathScanner(String res) {
         Scanner in = null;
         String path = In.class.getClassLoader().getResource(res).getFile();
@@ -31,4 +36,16 @@ public class In {
         }
         return in;
     }
+    
+    /**
+     * 类路径的指定类所在的包下获取资源的Scanner
+     * @param cl
+     * @param res
+     * @return
+     */
+    public static Scanner getClassPathScanner(Class<?> cl, String res) {
+        String pkgName = cl.getPackage().getName().replaceAll("\\.", "/");
+        return getClassPathScanner(pkgName + "/" + res) ;
+    }
+    
 }

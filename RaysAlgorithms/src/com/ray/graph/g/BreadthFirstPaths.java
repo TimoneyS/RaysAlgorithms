82ef09b0cbf6c15a812087cb236fc78cf1523ae5
rays1 <rays1@192.168.32.71>
2017-12-27 16:51:38 +0800

    广度优先搜索

--- RaysAlgorithms/src/com/ray/graph/g/BreadthFirstPaths.java
+++ RaysAlgorithms/src/com/ray/graph/g/BreadthFirstPaths.java
@@ -1,11 +1,13 @@
 package com.ray.graph.g;
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
 
 import com.ray.graph.common.Paths;
 
 /**
- * 广度优先搜索算法<br>
+ * 广度优先搜索<br>
  * 先将起点加入队列，然后重复一下步骤直到队列为空 <br>
  * 1.取队列的下一个顶点，并标记该顶点 <br>
  * 2.将与v相邻的所有未被标记的顶点加入队列
@@ -18,12 +20,14 @@ public class BreadthFirstPaths implements Paths {
     private boolean[]           marked; // 标记某个顶点搜索时是否已经被访问
     private int[]               edgeTo; // 存放至某点的一个邻接点
     private int                 s;      // 起点
-    private LinkedList<Integer> queue;
+    private Queue<Integer>      queue;
 
     public BreadthFirstPaths(Graph G, int s) {
+        
         marked = new boolean[G.V()];
         edgeTo = new int[G.V()];
-        queue = new LinkedList<Integer>();
+        queue = new ArrayDeque<Integer>(G.V());
+        
         this.s = s;
         queue.add(s);
         marked[s] = true;
@@ -31,7 +35,7 @@ public class BreadthFirstPaths implements Paths {
         search(G, s);
     }
 
-    public void search(Graph G, int s) {
+    private void search(Graph G, int s) {
         while (!queue.isEmpty()) {
             int v = queue.poll();
             for (int w : G.adj(v)) {
@@ -48,16 +52,16 @@ public class BreadthFirstPaths implements Paths {
         return marked[v];
     }
 
-    public LinkedList<Integer> pathTo(int v) {
+    public Stack<Integer> pathTo(int v) {
         if (!marked[v])
             return null;
-        LinkedList<Integer> list = new LinkedList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
 
         for (int x = v; x != s; x = edgeTo[x]) {
-            list.push(x);
+            stack.push(x);
         }
-        list.push(s);
-        return list;
+        stack.push(s);
+        return stack;
     }
 
 }

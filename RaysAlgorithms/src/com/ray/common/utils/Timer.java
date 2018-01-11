630caa843e84ffe5ea36961b151bace31c52ef5d
rays1 <rays1@Rays-PC>
2018-01-11 21:22:11 +0800

    计时器

--- RaysAlgorithms/src/com/ray/common/utils/Timer.java
+++ RaysAlgorithms/src/com/ray/common/utils/Timer.java
@@ -1,7 +1,5 @@
 package com.ray.common.utils;
 
-import static com.ray.common.utils.Out.*;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,41 +10,49 @@ import java.util.Date;
  */
 public class Timer {
 	
-	private static Date RECORD_DATE = null;// = new Date();
-	private static Integer RECORD_COUNT = 1;
-    private static SimpleDateFormat dateFormater = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss:ms]"); 
+    private static Date             RECORD_DATE;
+    private static Integer          RECORD_COUNT;
+    private static SimpleDateFormat DATE_FORMATTER;
 	
-	
+    static {
+        RECORD_COUNT   = 0;
+        DATE_FORMATTER = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss:ms]");
+    }
+    
 	/**
-	 * “点击” 计时器， 第一次点击 开始记录时间，之后每次按下将显示前一次的计时并开始下一次计时
+	 * "按下" 计时器<br/>
+	 * 第一次按下 开始记录时间，之后每次按下将显示距离上一次的时间，并开始下一次计时
 	 */
 	public static void click() {
-		if(RECORD_DATE == null) begin();
+		if(RECORD_COUNT == 0) begin();
 		else record();
 	}
 	
-	private static void begin() {
+	/**
+	 * "停止" 计时器
+	 */
+	public static final void stop() {
+	    record();
+    	RECORD_COUNT = 0;
+    	Out.p(tag() + "结束计时...");
+    }
+
+    private static void begin() {
+        Out.p(tag() + "开始计时...");
 		RECORD_DATE = new Date();
 		RECORD_COUNT = 1;
 	}
 	
 	private static void record() {
-		pf("第%2d次耗时记录：%s ms. \n", RECORD_COUNT ++, new Date().getTime() - RECORD_DATE.getTime());
+		Out.pf(tag()+"第%2d次耗时记录 : %sms.\n", RECORD_COUNT ++, new Date().getTime() - RECORD_DATE.getTime());
 		RECORD_DATE = new Date();	// 消除打印的时间差，因此在后
-	}
-	
-	public static final void stop() {
-		pf("第%2d次耗时记录：%s ms. \n", RECORD_COUNT ++, new Date().getTime() - RECORD_DATE.getTime());
-		RECORD_DATE  = null;
-		RECORD_COUNT = 1;
 	}
 	
 	/**
 	 * 时间戳
 	 */
 	public static String tag() {
-        return dateFormater.format(new Date());
-	    
+        return DATE_FORMATTER.format(new Date());
 	}
 	
 }

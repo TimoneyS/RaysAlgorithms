e29b805a27f265186a758e390c22652c8c4eb3c0
rays1 <rays1@Rays-PC>
2018-01-11 23:41:36 +0800

    排序基础类

--- RaysAlgorithms/src/com/rays/algo/sort/Sort.java
+++ RaysAlgorithms/src/com/rays/algo/sort/Sort.java
@@ -1,9 +1,11 @@
 package com.rays.algo.sort;
 
-public interface Sort<T extends Comparable<?>> {
+public abstract class Sort<T extends Comparable<?>> {
     
-    public void sort(T[] arr);
+    public void sort(T[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
     
-    public void sort(T[] arr, int lo, int hi);
+    public abstract void sort(T[] arr, int lo, int hi);
     
 }

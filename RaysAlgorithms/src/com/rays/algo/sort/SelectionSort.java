74822ecc8791e2c380e54b6799b06f7543e6acc9
rays1 <rays1@Rays-PC>
2018-01-11 23:36:49 +0800

    选择排序

--- RaysAlgorithms/src/com/rays/algo/sort/SelectionSort.java
+++ RaysAlgorithms/src/com/rays/algo/sort/SelectionSort.java
@@ -1,33 +1,40 @@
 package com.rays.algo.sort;
 
-import static com.ray.common.utils.ArrayUtil.*;
-
 /**
  * <b>选择排序</b>
  * <p>
- * 基本思想：<br/>
- * 1.遍历未排序的部分。<br/>
- * 2.找出最小元素，将其和未排序的第一个元素交换，该元素便已排序。<br/>
- * 3.重复 1 2 直到所有元素已排序。
+ * 基本思想 :<br/>
+ * 找出数组中的最小元素和数组的第一个元素交换<br/>
+ * 在余下的元素中找出最小数组和第二个元素交换<br/>
+ * ...
  * <p>
- * 算法特点：<br/>
+ * 算法特点 :<br/>
  * 1.运行时间和输入无关<br/>
  * 2.数据的移动量最少<br/>
  * @author Ray
  */
-public class SelectionSort {
+public class SelectionSort<T extends Comparable<?>> implements Sort<T> {
 
-	@SuppressWarnings({ "rawtypes" })
-	public static void sort(Comparable[] arr) {
-		
-		for(int i = 0; i < arr.length; i++) {
-			int minIdx = i; 
-			for(int j = i;  j < arr.length; j ++) {
-				if(less(arr, j, minIdx)) minIdx = j;
-			}
-			swap(arr, minIdx, i);
-		}
-		
-	}
+//	public void sort(Comparable[] arr) {
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			int minIdx = i; 
+//			for(int j = i;  j < arr.length; j ++) {
+//				if(less(arr, j, minIdx)) minIdx = j;
+//			}
+//			swap(arr, minIdx, i);
+//		}
+//		
+//	}
+
+    @Override
+    public void sort(T[] arr) {
+        
+    }
+
+    @Override
+    public void sort(T[] arr, int lo, int hi) {
+        
+    }
 
 }

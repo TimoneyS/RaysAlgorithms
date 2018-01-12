c893e96a127aaf0038189cbc0ec585e6dcc2a434
rays1 <rays1@Rays-PC>
2018-01-12 18:29:15 +0800

    插入排序

--- RaysAlgorithms/src/com/rays/algo/sort/InsertionSort.java
+++ RaysAlgorithms/src/com/rays/algo/sort/InsertionSort.java
@@ -1,6 +1,6 @@
 package com.rays.algo.sort;
 
-import static com.ray.common.utils.ArrayUtil.*;
+import com.ray.common.utils.ArrayUtil;
 
 /**
  * <b>插入排序</b>
@@ -8,21 +8,19 @@ import static com.ray.common.utils.ArrayUtil.*;
  * <p>
  * 基本思想：<br>
  * 1.插入排序中，当前元素左侧是已经排序的部分，右侧是未排序部分<br>
- * 2.用当前元素和左侧元素比较，如果比左侧元素小则执行交换，直到到达数组尽头或左侧元素不再更大。<br>
+ * 2.用当前元素和左侧元素比较，如果比左侧元素小则执行交换，直到到达数组首位或左侧元素不再更大。<br>
  * <br>
  * @author Ray
  */
-public class InsertionSort {
+public class InsertionSort<T extends Comparable<?>> extends Sort<T> {
 
-	@SuppressWarnings("rawtypes")
-	public static void sort(Comparable[] arr) {
-		
-		for (int i = 0; i < arr.length; i++) {
-			for(int j = i; j > 0 && less(arr, j, j-1); j --) {
-				swap(arr, j, j-1);
-			}
-		}
-
-	}
+    @Override
+    public void sort(T[] arr, int lo, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > 0 && ArrayUtil.less(arr, j, j-1); j --) {
+                ArrayUtil.swap(arr, j, j-1);
+            }
+        }
+    }
 	
 }

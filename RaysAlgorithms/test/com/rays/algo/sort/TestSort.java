d9be1b648bc80089a4d276129f4081589cd73afc
rays1 <rays1@Rays-PC>
2018-01-12 18:29:21 +0800

    测试方法

--- RaysAlgorithms/test/com/rays/algo/sort/TestSort.java
+++ RaysAlgorithms/test/com/rays/algo/sort/TestSort.java
@@ -11,38 +11,30 @@ import com.ray.common.utils.io.Out;
 public class TestSort {
 	
 	interface Case {public void run(Comparable<?>[] arr);}
+
+   public static void testInsertionSort() {
+        Integer[] arr = intArr(20);
+        ArrayUtil.shuffle(arr);
+        Out.p(arr);
+        testSort("—°‘Ò≈≈–Ú", new InsertionSort<Integer>(), arr);
+        Out.p(arr);
+    }
 	
 	public static void testSelectionSort() {
 	    Integer[] arr = intArr(20);
 	    ArrayUtil.shuffle(arr);
 	    Out.p(arr);
 	    testSort("—°‘Ò≈≈–Ú", new SelectionSort<Integer>(), arr);
-	    
 	    Out.p(arr);
 	}
 	
 	public static void main(String[] args) {
-	    testSelectionSort();
-//        runcase(arr, (a) -> HeapSort.sort(a));
-//        runcase(arr, (a) -> InsertionSort.sort(a));
-//        runcase(arr, (a) -> MergeSort.DownToUp.sort(a));
-//        runcase(arr, (a) -> QuickSort.sort(a));
-//        runcase(arr, (a) -> ShellSort.sort(a));
-        
+	    testInsertionSort();
     }
 	
 	private static <T extends Comparable<?>> void testSort(String name, Sort<T> s, T[] arr) {
 	    s.sort(arr);
         checkSorted(arr);
-	}
-	
-	public static void runcase(Comparable<?>[] arr, Case t) {
-        Timer timer = Timer.create();
-		shuffle(arr);
-		timer.click();
-		t.run(arr);
-		timer.stop();
-		checkSorted(arr);
 	}
 	
 }

911e6a6a454e8e8db169de72016e10600f9811b3
rays1 <rays1@Rays-PC>
2018-01-21 00:42:34 +0800

    归并排序

--- RaysAlgorithms/src/com/rays/algo/sort/MergeSort.java
+++ RaysAlgorithms/src/com/rays/algo/sort/MergeSort.java
@@ -8,7 +8,7 @@ import static com.ray.common.utils.ArrayUtil.*;
  * 基本思想：<br/>
  * 归并操作 - 将两个有序的数组合并为一个更大的有序数组<br/>
  * 在排序一个数组时，递归的将其拆解为两个数组排序，然后将两个子数组归并起来。<br/>
- * </p>
+ * </p>·
  * @author Ray
  */
 public class MergeSort {
@@ -27,32 +27,61 @@ public class MergeSort {
         
         private void mergeSort(T[] arr, int lo, int hi, Comparable<?>[] temp) {
             if (lo < hi) {
-                int mid = (lo + hi) / 2;
-                mergeSort(arr, lo, mid, temp);
-                mergeSort(arr, mid + 1, hi, temp);
-                merge(arr, lo, mid, hi, temp);
+                int mid = (lo + hi) / 2;            // 计算中点
+                mergeSort(arr, lo, mid, temp);      // 排序左侧子数组
+                mergeSort(arr, mid + 1, hi, temp);  // 排序右侧子数组
+                merge(arr, lo, mid, hi, temp);      // 归并
             }
         }
 		
 	}
 	
+	/**
+	 * 自底向上的归并排序
+	 * @author rays1
+	 *
+	 * @param <T>
+	 */
 	public static class DownToUp<T extends Comparable<?>> extends Sort<T> {
-
-//	    public static void sort(Comparable<?>[] a){
-//			Comparable<?>[] temp = new Comparable[a.length];
-//			int N = a.length;
-//			// size 表示子数组的长度，也就是每次合并的尺度
-//			for(int size = 1; size < N; size = size * 2)
-//				for(int l = 0; l < N-size; l = l + size * 2)
-//					merge(a, l, l+size-1, Math.min(N-1, l+2*size-1), temp);
-//		}
 
         @Override
         public void sort(T[] arr, int lo, int hi) {
-            
-            
+            Comparable<?>[] temp = new Comparable<?>[hi-lo+1];
+            int N = hi - lo + 1;
+            for (int size = 1; size < N; size *= 2) {                            // 幂次递增的归并尺寸
+                for (int l = lo; l < N-size; l += size*2 ) {                     // 循环处理每一对待归并的子数组
+                    merge(arr, l, (l+size-1), Math.min( l+size*2-1, hi), temp);  // 归并起点为l长度为size的两个相邻的子数组
+                }
+            }
         }
         
+	}
+	
+	/**
+	 * 小数组采用选择排序的归并排序
+	 * @author rays1
+	 *
+	 * @param <T>
+	 */
+	public static class DownToUpSelect<T extends Comparable<?>> extends Sort<T>  {
+        @Override
+        public void sort(T[] arr, int lo, int hi) {
+            Comparable<?>[] temp = new Comparable<?>[hi-lo+1];
+            Sort<T> s = new SelectionSort<>();
+            
+            int optiSize = 16;  // 切换选择排序和归并排序的尺寸
+            
+            int N = hi - lo + 1;
+            for (int i = 0; i < N-optiSize; i += optiSize) {
+                s.sort(arr, i, Math.min(i+optiSize-1, hi));
+            }
+
+            for (int size = optiSize; size < N; size *= 2) {                     // 幂次递增的归并尺寸
+                for (int l = lo; l < N-size; l += size*2 ) {                     // 循环处理每一对待归并的子数组
+                    merge(arr, l, (l+size-1), Math.min( l+size*2-1, hi), temp);  // 归并起点为l长度为size的两个相邻的子数组
+                }
+            }
+        }
 	}
 	
 	/**
@@ -61,14 +90,13 @@ public class MergeSort {
 	 * arr[mid+1] ~ arr[hi] 是已经排序的数组2<br>
 	 * 归并效果等同从两个独立排序的数组中依次获取较小元素，放入大数组中。<br>
 	 * 归并完成后，大数组为自然已排序状态。
-	 * @param arr   待排序数组
-	 * @param lo	下界
+	 * @param arr   待排序数组 
 	 * @param mid	中点
 	 * @param hi	上界
 	 */
 	private static void merge(Comparable<?>[] arr, int lo, int mid, int hi, Comparable<?>[] temp){
 		
-	    for(int i = lo; i <= hi; i++) temp[i] = arr[i];
+	    for(int i = lo; i <= hi; i++) temp[i] = arr[i];    // 缓存
 		
 	    int i = lo,     // 大数组索引
             l = lo,     // 左侧子数组索引

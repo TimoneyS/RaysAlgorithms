c1f1543df7daaeca4cba5f24e6c28f830c8fd4b5
rays1 <rays1@Rays-PC>
2018-01-21 21:11:02 +0800

    最大优先级队列

--- RaysAlgorithms/src/com/ray/common/utils/collections/MaxPQ.java
+++ RaysAlgorithms/src/com/ray/common/utils/collections/MaxPQ.java
@@ -1,5 +1,11 @@
 package com.ray.common.utils.collections;
 
-public interface MaxPQ {
+public interface MaxPQ<Key extends Comparable<Key>> {
+    
+    void insert(Key key);
+    Key max();
+    Key delMax();
+    boolean isEmpty();
+    int size();
     
 }

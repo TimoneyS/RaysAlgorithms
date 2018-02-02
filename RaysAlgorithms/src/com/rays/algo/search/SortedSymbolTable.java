67f413fe73ea74306743caeb9945827e2b13f2de
rays1 <rays1@Rays-PC>
2018-02-02 20:45:29 +0800

    符号表API

--- RaysAlgorithms/src/com/rays/algo/search/SortedSymbolTable.java
+++ RaysAlgorithms/src/com/rays/algo/search/SortedSymbolTable.java
@@ -1,11 +1,11 @@
 package com.rays.algo.search;
 
 public interface SortedSymbolTable <Key extends Comparable<Key>,Value> extends SymbolTable<Key, Value> {
-	Key min();
-	Key max();
-	Key floor(Key key);
-	Key celing(Key key);
-    int rank(Key key);
-    Key select(int index);
-    Key[] keys(int lo, int hi);
+    public Key min();
+    public Key max();
+    public Key floor(Key key);
+    public Key celing(Key key);
+    public int rank(Key key);
+    public Key select(int index);
+    public Key[] keys(int lo, int hi);
 }

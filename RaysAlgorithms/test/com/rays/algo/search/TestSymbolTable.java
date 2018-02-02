c12b1ae2cbe2adce8f844c77ab479cd30c10cf3e
rays1 <rays1@Rays-PC>
2018-02-02 20:44:27 +0800

    链表实现的顺序查找符号表

--- RaysAlgorithms/test/com/rays/algo/search/TestSymbolTable.java
+++ RaysAlgorithms/test/com/rays/algo/search/TestSymbolTable.java
@@ -1,42 +1,39 @@
 package com.rays.algo.search;
 
 import static com.ray.common.util.ArrayUtil.intArr;
-import static com.ray.common.util.ArrayUtil.shuffle;
 
 import com.ray.common.io.Out;
 import com.ray.common.util.Timer;
 
 public class TestSymbolTable {
 	
-	public static void test(int size, SymbolTable<Integer, Integer> st) {
-		Out.p(">Test " + st.getClass().getSimpleName());
-		Integer[] arr = intArr(size);
-		shuffle(arr);
-		Timer t = Timer.create();
-		t.click();
-		for(Integer i : arr) {
-			st.put(i, i*10);
-		}
-		t.click();
-		for(Integer i = 0; i < size; i++) {
-			st.get(i);
-		}
-		t.stop();
-	}
-	
+    /**
+     * 测试链表实现的顺序查找的符号表
+     */
+    public static void testSequentialSearchST() {
+        int size = 10;
+        SymbolTable<Integer, Integer> st = new SequentialSearchST<>();
+        Timer t = Timer.create();
+        
+        Integer[] arr = intArr(size);
+        t.click();
+        for(Integer i : arr) st.put(i, i);
+        Out.p(st.size());
+        t.click();
+        for(Integer i = size; i > 0; i--) st.get(i);
+        t.stop();
+    }
+    
 	public static void main(String[] args) {
-		int size = 1000000;
+	    testSequentialSearchST();
 		
 //		test(size, new BinarySearch<>(size));
 		
-		test(size, new BinarySearchTree<>());
+//		test(size, new BinarySearchTree<>());
 		
-		test(size, new HashSymbolTable<>(size/10));
+//		test(size, new HashSymbolTable<>(size/10));
 		
-		test(size, new RedBlackBinarySearchTree<>());
-		
-//		test(size, new SequentialSearch<>());
-		
+//		test(size, new RedBlackBinarySearchTree<>());
 		
 	}
 	

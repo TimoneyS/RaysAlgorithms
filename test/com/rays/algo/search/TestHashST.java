517de92ed5fb9e52d2dde83ee33516ad57262d89
Ray <Rays1990@163.com>
2018-04-24 18:24:49 +0800

    拉链法实现的散列表

---
+++ test/com/rays/algo/search/TestHashST.java
@@ -0,0 +1,12 @@
+package com.rays.algo.search;
+
+import com.ray.util.io.Out;
+
+public class TestHashST {
+    
+    public static void main(String[] args) {
+        
+        Out.p("kjfewkvnewlknvlkjvewknvlkewnvhjewbkjwbfjewvnfwqoewihve".hashCode());
+    }
+    
+}

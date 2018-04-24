3ec15c1c8fc6bf8f232d83679edadd482cc27625
Ray <Rays1990@163.com>
2018-04-24 21:56:32 +0800

    优化

--- src/com/rays/algo/graph/g/BreadthFirstPaths.java
+++ src/com/rays/algo/graph/g/BreadthFirstPaths.java
@@ -6,18 +6,17 @@ import java.util.Stack;
 
 /**
  * 广度优先搜索<br>
- * 先将起点加入队列，然后重复一下步骤直到队列为空 <br>
- * 1.取队列的下一个顶点，并标记该顶点 <br>
- * 2.将与v相邻的所有未被标记的顶点加入队列
- * 
+ *  循环访问队列所有顶点，在访问其中一个顶点时<br>
+ *    1.获取其所有未被标记的顶点<br>
+ *    2.将符合的相邻的顶点加入队列并标记<br>
  * @author rays1
  *
  */
 public class BreadthFirstPaths implements Paths {
 
-    private boolean[]           marked; // 标记某个顶点搜索时是否已经被访问
-    private int[]               edgeTo; // 存放至某点的一个邻接点
-    private int                 s;      // 起点
+    private boolean[]           marked;     // 标记某个顶点搜索时是否已经被访问
+    private int[]               edgeTo;     // 存放至某点的一个邻接点
+    private int                 start;      // 起点
     private Queue<Integer>      queue;
 
     public BreadthFirstPaths(Graph G, int s) {
@@ -26,39 +25,40 @@ public class BreadthFirstPaths implements Paths {
         edgeTo = new int[G.V()];
         queue = new ArrayDeque<Integer>(G.V());
         
-        this.s = s;
+        this.start = s;
         queue.add(s);
         marked[s] = true;
-        
         search(G, s);
     }
 
     private void search(Graph G, int s) {
         while (!queue.isEmpty()) {
             int v = queue.poll();
+            
             for (int w : G.adj(v)) {
-                if (!marked[w]) {
-                    edgeTo[w] = v;
-                    marked[w] = true;
-                    queue.add(w);
-                }
+                if (marked[w]) continue;
+                edgeTo[w] = v;
+                queue.add(w);
+                marked[w] = true;
             }
         }
     }
 
+    @Override
     public boolean hasPathTo(int v) {
         return marked[v];
     }
 
+    @Override
     public Stack<Integer> pathTo(int v) {
         if (!marked[v])
             return null;
         Stack<Integer> stack = new Stack<Integer>();
 
-        for (int x = v; x != s; x = edgeTo[x]) {
+        for (int x = v; x != start; x = edgeTo[x]) {
             stack.push(x);
         }
-        stack.push(s);
+        stack.push(start);
         return stack;
     }
 

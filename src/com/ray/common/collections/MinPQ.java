70f4947a470006d14e6603cefac7b873071fd818
Ray <Rays1990@163.com>
2018-06-18 21:30:24 +0800

    change

--- src/com/ray/common/collections/MinPQ.java
+++ src/com/ray/common/collections/MinPQ.java
@@ -1,41 +1,43 @@
 package com.ray.common.collections;
 
 /**
+ * 
  * 最小优先级队列
  * @author rays1
  *
  * @param <Key>
+ * 
  */
 public interface MinPQ<Key extends Comparable<Key>> {
-    
+
     /**
      * 插入元素
      * @param key
      */
     void insert(Key key);
-    
+
     /**
      * 获取最小元素
      * @return
      */
     Key min();
-    
+
     /**
      * 删除最小元素
      * @return
      */
     Key delMin();
-    
+
     /**
      * 队列是否为空
      * @return
      */
     boolean isEmpty();
-    
+
     /**
      * 队列中元素个数
      * @return
      */
     int size();
-    
+
 }
--- src/com/ray/common/collections/RaysMinPQ.java
+++ src/com/ray/common/collections/RaysMinPQ.java
@@ -11,44 +11,44 @@ import com.ray.util.Timer;
 
 /**
  * 个人实现的最小优先级队列
+ * 
+ * 内部使用数据表示的二叉树保存数据
+ * 
+ *              4
+ *           /     \
+ *          2       6
+ *        /   \   /   \
+ *       1     3 5     7   
+ * 
  * @author rays1
  *
  * @param <Key>
  */
 public class RaysMinPQ<Key extends Comparable<Key>> implements MinPQ<Key> {
 
+    private Key[] inner;            // 内部数组，保存二叉树
+    private int   cursor;           // 游标，无元素指向为0，有元素后指向最后一个元素
+
     public RaysMinPQ() {
         this(1);
     }
-    
+
     @SuppressWarnings("unchecked")
     public RaysMinPQ(int capacity) {
         inner = (Key[]) new Comparable[capacity+1];
         cursor = 0;
     }
-    
+
     @Override
     public void insert(Key key) {
         resize();
         inner[++cursor] = key;  // 新元素放入末尾
         swim(cursor);           // 对新元素执行上浮
     }
-    
-    @SuppressWarnings("unchecked")
-    public void resize() {
-        if (cursor+1 < inner.length) return;
-        
-        int newSize = inner.length * 2;
-        Key[] temp = inner;
-        inner = (Key[]) new Comparable[newSize];
-        for (int i = 0; i < temp.length; i++) {
-            inner[i] = temp[i];
-        }
-    }
-    
+
     @Override
     public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        if (isEmpty()) return null;
         return inner[1];
     }
 
@@ -69,21 +69,34 @@ public class RaysMinPQ<Key extends Comparable<Key>> implements MinPQ<Key> {
     public int size() {
         return cursor;
     }
-    
-    private Key[] inner;            // 内部数组，保存二叉树
-    private int   cursor;           // 游标，无元素指向为0，有元素后指向最后一个元素
-    
+
+    /*********************
+     * (辅助方法)重新调整内部数组的尺寸
+     *********************/
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        if (cursor+1 < inner.length) return;
+        
+        int newSize = inner.length * 2;
+        Key[] temp = inner;
+        inner = (Key[]) new Comparable[newSize];
+        for (int i = 0; i < temp.length; i++) {
+            inner[i] = temp[i];
+        }
+    }
+
     /**************************************
      * (辅助方法)上浮操作
      * @param index
      **************************************/
     private void swim(int index) {
-        while(index > 1 && less(inner, index, index/2)) {     // 未到达根节点或者父节点的元素比当前元素小
-            swap(inner, index, index/2);                      // 交换当前元素和其父节点
-            index/=2;                                         // 当前元素索引修正
+        int fIndex = index/2;
+        while(index > 1 && less(inner, index, fIndex)) {     // 未到达根节点或者父节点的元素比当前元素小
+            swap(inner, index, fIndex);                      // 交换当前元素和其父节点
+            index = fIndex;                                  // 当前元素索引修正
         }
     }
-    
+
     /**************************************
      * (辅助方法)下沉操作
      * @param index
@@ -93,22 +106,22 @@ public class RaysMinPQ<Key extends Comparable<Key>> implements MinPQ<Key> {
             int childIndex = index*2;                                 // 子节点索引
             if(less(inner, childIndex+1, childIndex)) childIndex ++;  // 选取较大的子节点的索引
             if(less(inner, childIndex, index)) {                      // 子节点更大
-                swap(inner, childIndex, index);                      
+                swap(inner, childIndex, index);
                 index = childIndex;
             } else {                                                  // 当前元素大于子节点
                 break;
             }
         }
     }
-    
+
     public static void main(String[] args) {
         MinPQ<Integer> pq = new RaysMinPQ<Integer>();
-        
+
         int size = 20;
         Random r = new Random(42);
-        
+
         Timer t = Timer.create();
-        
+
         t.click();
         for (int i = 0; i < size; i++) {
             pq.insert(r.nextInt(size * 10));

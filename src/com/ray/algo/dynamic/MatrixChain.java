09b11f0a705f5e224038847b473f6f41b79e3ad0
Ray <Rays1990@163.com>
2018-07-08 11:23:45 +0800

    矩阵连乘

--- src/com/ray/algo/dynamic/MatrixChain.java
+++ src/com/ray/algo/dynamic/MatrixChain.java
@@ -1,6 +1,9 @@
 package com.ray.algo.dynamic;
 
+import java.util.Random;
+
 import com.ray.io.Out;
+import com.ray.util.Timer;
 
 /**
  * 矩阵连乘问题
@@ -9,42 +12,56 @@ import com.ray.io.Out;
  */
 public class MatrixChain {
     
+    private int[][] A;
+    private int[][] S;
+    
     public MatrixChain(int[] p) {
         
         int n = p.length-1;
         
-        int[][] A = new int[n+1][n+1];  // A[i][j]表示矩阵链中，从 A[i] 到  A[j] 的最小计算次数
-        int[][] S = new int[n+1][n+1];  // S[i][j]表示矩阵连中，从 A[i] 到  A[j] 的断开位置
-        
+        A = new int[n+1][n+1];  // A[i][j]表示矩阵链中，从 A[i] 到  A[j] 的最小计算次数
+        S = new int[n+1][n+1];  // S[i][j]表示矩阵连中，从 A[i] 到  A[j] 的断开位置
         // 初始化
         for (int i = 1; i <= n; i++) A[i][i] = 0;
         
-        for (int r = 2; r <= n; r++) {
-            for (int i = 1; i <= n - r +1; i++) {
-                
-                int j = i + r - 1;
-                
-                A[i][j] = A[i+1][j] + p[i-1]*p[i]*p[j];
-                S[i][j] = i;
-                
-                for (int k = i+1; k < j; k++) {
-                    int t = A[i][k] + A[k+1][j] + p[i-1]*p[k]*p[j];
-                    if (t < A[i][j]) {
-                        A[i][j] = t;
-                        S[i][j] = k;
-                    }
-                }
-                
-            }
+        for (int r = 1; r < n; r ++) {
+            for (int i = 1; i <= n - r;i ++) {
+                int j = i + r;
+                calcu(p, i, j);
+            } 
         }
-        
-        Out.p(split(S, 1, n));
         
         Out.p(A[1][n]);
         
     }
     
-    private String split(int[][] S, int i, int j) {
+    /**
+     * 内部计算
+     * @param p
+     * @param i
+     * @param j
+     */
+    private void calcu(int[] p, int i, int j) {
+        A[i][j] = A[i+1][j] + p[i-1]*p[i]*p[j];
+        S[i][j] = i;
+        
+        for (int k = i+1; k < j; k++) {
+            int t = A[i][k] + A[k+1][j] + p[i-1]*p[k]*p[j];
+            if (t < A[i][j]) {
+                A[i][j] = t;
+                S[i][j] = k;
+            }
+        }
+    }
+    
+    /**
+     * 计算结果
+     * @param S
+     * @param i
+     * @param j
+     * @return
+     */
+    public String split(int[][] S, int i, int j) {
         if (i == j) return "A" + i;
         
         int k = S[i][j];
@@ -90,10 +107,17 @@ public class MatrixChain {
 
     public static void main(String[] args) {
         
-        int[] p = {3,8,2,1,4,8,7,5,1,9,2,8,4,2,4};
+        int size  = 10;
+        Random r = new Random(42);
+        int[] p = new int[size];
+        for (int i = 0; i < size; i++) {
+            p[i] = r.nextInt(5)+1;
+        }
+        Timer t =  Timer.create();
         
+        t.click();
         new MatrixChain(p);
-        
+        t.stop();
     }
 
 }

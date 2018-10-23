3ffc07945084132c11c60de31cc2daf93e81631d
Ray <Rays1990@163.com>
2018-10-23 23:13:51 +0800

    ADD

---
+++ src/com/ray/algorithms/dynamic/SeamCarving.java
@@ -0,0 +1,22 @@
+package com.ray.algorithms.dynamic;
+
+/**
+ * 基于接缝裁剪的图像压缩
+ * @author rays1
+ *
+ */
+public class SeamCarving {
+    
+    public SeamCarving(String[][] pic) {
+        
+        int m = pic.length;
+        int n = pic[0].length;
+        
+        
+        
+        
+    }
+    
+    
+    
+}

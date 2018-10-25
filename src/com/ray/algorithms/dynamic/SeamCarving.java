10318e8b8b68f5a1f3ce060ce42016d90084f4e7
Ray <Rays1990@163.com>
2018-10-25 16:30:34 +0800

    seam carving

--- src/com/ray/algorithms/dynamic/SeamCarving.java
+++ src/com/ray/algorithms/dynamic/SeamCarving.java
@@ -13,6 +13,7 @@ import com.ray.io.Out;
  */
 public class SeamCarving {
     
+    private int maxSeamEnergy;
     private int[][] energy;
     private int[][] picture;
     private int width;
@@ -23,17 +24,23 @@ public class SeamCarving {
         width = picture.width();
         height = picture.height();
         
-        energy = new int[height][width];
-        this.picture = new int[height][width];
+        maxSeamEnergy = Math.max(width, height) * 1000;
+        
+        energy = new int[width][height];
+        this.picture = new int[width][height];
         
         // 处理图片到 RGB 格式
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                this.picture[i][j] = picture.getRGB(j, i);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.picture[i][j] = picture.getRGB(i, j);
             }
         }
-        
-        computeEnergy();
+        // 计算能量
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                energy[i][j] = computePixelEnergy(i, j);
+            }
+        }
         
     }
     
@@ -44,32 +51,29 @@ public class SeamCarving {
     public int[] verticalSeamCarving() {
         
         int[] seam = new int[height];
-        int[][] seamEnergy = new int[height][width];
-        int[][] pathTo = new int[height][width];
+        int[][] seamEnergy = new int[width][height];
+        int[][] pathTo = new int[width][height];
         
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-//                if (i == 0)
-//                    seamEnergy[i][j] = energy[i][j];
-//                else
-                    seamEnergy[i][j] = -1;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                seamEnergy[i][j] = -1;
             }
         }
         
-        for (int j = 0; j < width; j ++) {
-            seamEnergy(0, j, seamEnergy, pathTo);
+        for (int i = 0; i < width; i ++) {
+            seamEnergy(i, 0, seamEnergy, pathTo);
         }
         
         int minIndex = 0;
         for (int i = 0; i < width; i++) {
-            if (seamEnergy[0][minIndex] > seamEnergy[0][i]) {
+            if (seamEnergy[minIndex][0] > seamEnergy[i][0]) {
                 minIndex = i;
             }
         }
         
         seam[0] = minIndex;
         for (int i = 1; i < height; i ++) {
-            seam[i] = pathTo[i][seam[i-1]];
+            seam[i] = pathTo[seam[i-1]][i];
         }
         
         return seam;
@@ -78,42 +82,29 @@ public class SeamCarving {
     public int[] horizontalSeamCarving() {
         return null;
     }
-    
-    public void showSeam(Picture pic, int[] seam) {
-        for (int j = 0; j < height; j++) {
-            int i = seam[j];
-            pic.set(i, j, Color.WHITE);
-        }
-    }
 
     private int seamEnergy(int i, int j, int[][] seamEnergy, int[][] pathTo) {
         
+        if (i < 0 || i >= width) return maxSeamEnergy;
+        
         if (seamEnergy[i][j] == -1) { // 没有备忘
             
-            if (i == height-1) {
+            if (j == height-1) { // 最后一行
                 seamEnergy[i][j] = energy[i][j];
             } else {
-                int a = 20000, b = 20000, c = 20000;
                 
-                if (j > 0)
-                    a = seamEnergy(i + 1, j - 1, seamEnergy, pathTo);
+                int a = seamEnergy(i - 1, j + 1, seamEnergy, pathTo);
+                int b = seamEnergy(    i, j + 1, seamEnergy, pathTo);
+                int c = seamEnergy(i + 1, j + 1, seamEnergy, pathTo);
                 
-                b = seamEnergy(i+1, j, seamEnergy, pathTo);
-                
-                if (j < width-1)
-                    c = seamEnergy(i + 1, j + 1, seamEnergy, pathTo);
-                
-                if (a <= b && a <= c) {
-                    // 左侧最小
-                    pathTo[i][j] = j-1;
+                if (a <= b && a <= c) { // 左侧最小
+                    pathTo[i][j] = i - 1;
                     seamEnergy[i][j] = energy[i][j] + a;
-                } else if (b <= c && b <= a) {
-                    // 中间最小
-                    pathTo[i][j] = j;
+                } else if (b <= c && b <= a) { // 中间最小
+                    pathTo[i][j] = i;
                     seamEnergy[i][j] = energy[i][j] + b;
-                } else {
-                    // 右侧最小
-                    pathTo[i][j] = j+1;
+                } else { // 右侧最小
+                    pathTo[i][j] = i + 1;
                     seamEnergy[i][j] = energy[i][j] + c;
                 }
                 
@@ -122,48 +113,52 @@ public class SeamCarving {
         return seamEnergy[i][j];
     }
     
-  public void removeVerticalSeam(int[] seam) {
-        
+    
+    public void showSeam(Picture pic, int[] seam) {
+        for (int i = 0; i < height; i++) {
+            int j = seam[i];
+            pic.set(i, j, Color.WHITE);
+        }
+    }
+    
+    /**
+     * 裁剪接缝
+     * @param seam
+     */
+    public void removeVerticalSeam(int[] seam) {
+
         int min = Integer.MAX_VALUE;
         int max = 0;
 
-        for (int i = 0; i < height; i++) {
-            if (seam[i] > max)
-                max = seam[i];
-            if (seam[i] < min)
-                min = seam[i];
+        for (int j = 0; j < height; j++) {
+            if (seam[j] > max)
+                max = seam[j];
+            if (seam[j] < min)
+                min = seam[j];
 
-            for (int j = seam[i]; j < width - 1; j++) {
-                picture[i][j] = picture[i][j+1];
+            for (int i = seam[j]; i < width - 1; i++) {
+                picture[i][j] = picture[i+1][j];
             }
         }
 
-        width --;
+        width--;
         if (min > 0)
-            min--;
+            min --;
         if (max > height - 1)
             max = height - 1;
 
-        for (int i = 0; i < height; i++) {
-            for (int j = min; j <= max; j++)
-                energy[i][j] = computePixelEnergy(i, j);
-            for (int j = max + 1; j < height - 1; j++)
-                energy[i][j] = energy[i][j+1];
+        for (int j = 0; j < height; j++) {
+            for (int i = min; i <= max; i++)
+                energy[j][i] = computePixelEnergy(j, i);
+            for (int i = max + 1; i < height - 1; i++)
+                energy[j][i] = energy[j][i + 1];
         }
-        
-    }
-    
-    public void computeEnergy() {
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                energy[i][j] = computePixelEnergy(i, j);
-            }
-        }
+
     }
     
     private int computePixelEnergy(int i, int j) {
         
-        if (i == 0 || j == 0 || i == height-1 || j == width - 1) {
+        if (i == 0 || j == 0 || i == width - 1 || j == height - 1) {
             return 1000;
         }
         
@@ -180,20 +175,38 @@ public class SeamCarving {
         int gy = Math.abs(((rgbUp >> 8) & 0xFF) - ((rgbDown >> 8) & 0xFF));
         int by = Math.abs(((rgbUp >> 0) & 0xFF) - ((rgbDown >> 0) & 0xFF));
         
-        return rx + gx + bx + ry + gy + by;
-    }
+        return (int) Math.sqrt(rx*rx + gx*gx + bx*bx + ry*ry + gy*gy + by*by);
+        
+//        return rx + gx + bx + ry + gy + by;
+    } 
     
     // current picture
     public Picture picture() {
         Picture pic = new Picture(width, height);
-        for (int i = 0; i < height; i++)
-            for (int j = 0; j < width; j++)
-                pic.setRGB(j, i, picture[i][j]);
-
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                pic.setRGB(i, j, picture[i][j]);
         return pic;
     }
     
     public static void main(String[] args) {
+        
+//        Picture pic = new Picture(9, 9);
+//
+//        
+//        pic.set(4, 4, new Color(255,255,255));
+//        
+//        pic.set(4, 3, new Color(0,0,0));
+//        pic.set(4, 5, new Color(255,255,255));
+//        pic.set(3, 4, new Color(0,0,0));
+//        pic.set(5, 4, new Color(255,255,255));
+//        
+//        SeamCarving sc = new SeamCarving(pic);
+//        
+//        Out.p(sc.picture, "%9s ");
+//        Out.sep();
+//        Out.p(sc.energy, "%4s ");
+        
         
         Picture pic = new Picture(In.getClassPathResource(SeamCarving.class, "light.jpg"));
         

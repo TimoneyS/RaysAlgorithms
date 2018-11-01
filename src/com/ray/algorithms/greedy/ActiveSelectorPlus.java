fb9c52655b96703eef1410ac464c4495508b8ae7
Ray <Rays1990@163.com>
2018-11-01 22:50:15 +0800

    Add

--- src/com/ray/algorithms/greedy/ActiveSelectorPlus.java
+++ src/com/ray/algorithms/greedy/ActiveSelectorPlus.java
@@ -63,8 +63,6 @@ public class ActiveSelectorPlus {
             handleEvent(i);
         }
         
-        Out.p(activeHall);
-        
     }
     
     /**
@@ -111,6 +109,8 @@ public class ActiveSelectorPlus {
         int[] s = {1, 3, 0, 5, 3, 5,  6,  8,  8,  2, 12};
         int[] f = {4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16};
         ActiveSelectorPlus asp = new ActiveSelectorPlus(s, f);
+        
+        Out.p(asp.activeHall);
         
     }
     

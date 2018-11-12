5094df6677f496bdc14f97b06abfea383f4effc8
Ray <Rays1990@163.com>
2018-11-12 14:10:02 +0800

    Add

---
+++ src/com/ray/LintCode/L_0013_Implement_strStr.java
@@ -0,0 +1,19 @@
+package com.ray.LintCode;
+
+/**
+ * For a given source string and a target string, 
+ * you should output the first index(from 0) of target string in source string.
+ * If target does not exist in source, just return -1.
+ * <p>
+ * Example
+ * <p>
+ * <li>If source = "source" and target = "target", return -1.
+ * <li>If source = "abcdabcdefg" and target = "bcd", return 1.
+ * <p>
+ * Challenge : O(n²) is acceptable. Can you implement an O(n) algorithm? (hint: KMP)
+ * @author rays1
+ *
+ */
+public class L_0013_Implement_strStr {
+
+}

9249fcd8b1ca400d78beea774634e200c2b1e768
Ray <Rays1990@163.com>
2018-11-17 19:48:10 +0800

    Add

--- src/com/ray/LintCode/L_0018_Subsets_II.java
+++ src/com/ray/LintCode/L_0018_Subsets_II.java
@@ -1,5 +1,11 @@
 package com.ray.LintCode;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.ray.io.Out;
+
 /**
  * Given a collection of integers that might contain duplicates, nums,
  * return all possible subsets (the power set).
@@ -15,5 +21,81 @@ package com.ray.LintCode;
  *
  */
 public class L_0018_Subsets_II {
-
+    
+    static class Solution {
+        
+        /**
+         * @param nums: A set of numbers.
+         * @return: A list of lists. All valid subsets.
+         */
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            // write your code here
+            List<List<Integer>> rs = new LinkedList<List<Integer>>();
+            
+            int n = nums.length;
+            
+            Arrays.sort(nums);
+            
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i] == nums[i-1]) n --;
+            }
+            
+            int[] marked  = new int[n];
+            int[] count   = new int[n];
+            int[] newNums = new int[n];
+            
+            build(nums, newNums, count);
+            
+            int k = 1;
+            for (int i = 0; i < count.length; i++) {
+                k *= (count[i] + 1);
+            }
+            
+            for (int i = 0; i < k; i++) {
+                Out.p(marked);
+                
+                List<Integer> l = new LinkedList<Integer>();
+                for (int j = 0; j < n; j++) {
+                    for (int j2 = 0; j2 < marked[j]; j2++) {
+                        l.add(newNums[j]);
+                    }
+                }
+                rs.add(l);
+                markedChange(marked, count);
+            }
+            
+            return rs;
+        }
+        
+        public void build (int[] nums, int[] newNums, int[] count) {
+            int cursor = 0;
+            for (int i = 0; i < nums.length; i++) {
+                newNums[cursor] = nums[i];
+                count[cursor] ++;
+                if (i < nums.length - 1 && nums[i+1] != nums[i])
+                    cursor ++;
+            }
+        }
+        
+        public void markedChange(int[] marked, int[] count) {
+            for (int i = 0; i < marked.length; i++) {
+                if (marked[i] != count[i]) {
+                    marked[i] += 1;
+                    return;
+                }
+                marked[i] = 0;
+            }
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        int[] nums = {1,2,2};
+        
+        for (List<Integer> l : new Solution().subsetsWithDup(nums)) {
+            Out.p(l);
+        }
+    }
+    
 }

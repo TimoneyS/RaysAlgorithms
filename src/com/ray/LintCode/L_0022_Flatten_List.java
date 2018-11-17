7a117cf0720126d97584987ea0d1e82f756c6846
Ray <Rays1990@163.com>
2018-11-17 23:47:42 +0800

    Add

--- src/com/ray/LintCode/L_0022_Flatten_List.java
+++ src/com/ray/LintCode/L_0022_Flatten_List.java
@@ -2,6 +2,11 @@ package com.ray.LintCode;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
+
+import com.ray.io.In;
+import com.ray.io.Out;
+import com.ray.util.Timer;
 
 /**
  * Given a list, each element in the list can be a list or integer.
@@ -61,18 +66,89 @@ public class L_0022_Flatten_List {
         }
     }
     
-    public interface NestedInteger {
+    static class NestedInteger {
+        
+        int num;
+        List<NestedInteger> innerList;
+        
         // @return true if this NestedInteger holds a single integer,
         // rather than a nested list.
-        public boolean isInteger();
+        public boolean isInteger() {
+            return innerList == null;
+        }
         // @return the single integer that this NestedInteger holds,
         // if it holds a single integer
         // Return null if this NestedInteger holds a nested list
-        public Integer getInteger();
+        public Integer getInteger() {
+            return num;
+        }
         // @return the nested list that this NestedInteger holds,
         // if it holds a nested list
         // Return null if this NestedInteger holds a single integer
-        public List<NestedInteger> getList();
+        public List<NestedInteger> getList() {
+            return innerList;
+        }
+        
+    }
+    
+    public static int build(NestedInteger ni, String s, int cursor ) {
+        ni.innerList = new LinkedList<NestedInteger>();
+        
+        while (cursor < s.length()) {
+            char c = s.charAt(cursor);
+            
+            if (c == '[') {
+                NestedInteger niT = new NestedInteger();
+                cursor = build(niT, s, cursor + 1);
+                ni.innerList.add(niT);
+            } else if (c == ',') {
+                cursor ++;
+            } else if (c == ']' ) {
+                cursor ++;
+                break;
+            } else if (c <= 57 && c >= 48) {
+                int num = c - 48;
+                while (cursor < s.length()) {
+                    char cc = s.charAt(cursor + 1);
+                    if (cc <= 57 && cc >= 48) {
+                        num = num * 10 + (cc-48);
+                        cursor += 1;
+                    } else {
+                        break;
+                    }
+                }
+                cursor += 1;
+                NestedInteger nit = new NestedInteger();
+                nit.num = num;
+                ni.innerList.add(nit);
+            }
+            
+        }
+        
+        return cursor;
+        
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = In.getClassPathScanner(L_0022_Flatten_List.class, "L_0022_Flatten_List.in");
+        StringBuilder sb = new StringBuilder();
+        
+        while (sc.hasNextLine()) {
+            sb.append(sc.nextLine());
+        }
+        
+        NestedInteger ni = new NestedInteger();
+        
+        Timer t = Timer.create();
+        
+        t.click();
+        build(ni, sb.toString(), 1);
+        t.click();
+        List<Integer> li = new Solution().flatten(ni.innerList);
+        t.stop();
+        
+        Out.p(li);
+        
     }
     
 }

6f81c85d57a5253c47effc707f5ca449358a4730
Ray <Rays1990@163.com>
2018-11-20 21:41:51 +0800

    Add

--- src/com/ray/LintCode/L_0029_Interleaving_String.java
+++ src/com/ray/LintCode/L_0029_Interleaving_String.java
@@ -17,9 +17,10 @@ import com.ray.io.Out;
  *
  */
 public class L_0029_Interleaving_String {
+    
+    
     static class Solution {
         
-        String s1, s2, s3;
         int l1, l2;
         int[][] A;
         
@@ -30,56 +31,73 @@ public class L_0029_Interleaving_String {
          * @return: Determine whether s3 is formed by interleaving of s1 and s2
          */
         public boolean isInterleave(String s1, String s2, String s3) {
-            // write your code here
-            
-            this.s1 = s1;
-            this.s2 = s2;
-            this.s3 = s3;
-            
             l1 = s1.length();
             l2 = s2.length();
-            
             A = new int[l1+1][l2+1];
             if (l1 + l2 != s3.length()) return false;
-            
-            return isInterleave(0, 0);
+            return isInterleave(s1,s2,s3,0, 0);
         }
         
-        
-        public boolean isInterleave(int i, int j) {
+        public boolean isInterleave(String s1, String s2, String s3, int i, int j) {
             if (i == l1 && j == l2) return true;
             if (A[i][j] != 0) return A[i][j] > 0;
-            
-            int c1 = i < l1 ? s1.charAt(i) : -1;
-            int c2 = j < l2 ? s2.charAt(j) : -1;
             int c3 = s3.charAt(i+j);
-            
             boolean rs = false;
-            if (c1 == c3 && c2 == c3) {
-                boolean rs1 = isInterleave(i, j+1);
-                boolean rs2 = isInterleave(i+1, j);
-                rs = (rs1 || rs2);
-            } else if (c1 == c3) {
-                rs = isInterleave(i+1, j);
-            } else if (c2 == c3) {
-                rs = isInterleave(i, j+1);
-            }
-            
+            if (i < l1 && s1.charAt(i) == c3) rs |= isInterleave(s1,s2,s3,i+1, j);
+            if (j < l2 && s2.charAt(j) == c3) rs |= isInterleave(s1,s2,s3,i, j+1);
             A[i][j] = rs ? 1 : -1;
             return rs;
-            
         }
         
     }
     
+    /**
+     * 动态规划问题
+     * 
+     * A[i][j] 表示 s1[0, i-1]，s2[0, j-1]，能否交叉构成  s3[0, i+j-1]
+     * 若能则有两种情况
+     * <li> 如果 s3[i+j] = s1[i]，那么  s1[0, i]，s2[0, j-1]，一定能构成  s3[0, i+j]，则 A[i+1][j] = true
+     * <li> 如果 s3[i+j] = s2[j]，那么  s1[0, i-1]，s2[0, j]，一定能构成  s3[0, i+j]，则 A[i][j+1] = true
+     * 
+     * @author rays1
+     *
+     */
+    static class Solution2 {
+        
+        /**
+         * @param s1: A string
+         * @param s2: A string
+         * @param s3: A string
+         * @return: Determine whether s3 is formed by interleaving of s1 and s2
+         */
+        public boolean isInterleave(String s1, String s2, String s3) {
+            // write your code here
+            if (s1.length() + s2.length() != s3.length()) return false;
+            
+            boolean[][] A = new boolean[s1.length() + 1][s2.length() + 1];
+            A[0][0] = true;
+            for (int i = 0; i <= s1.length(); i++) {
+                for (int j = 0; j <= s2.length(); j++) {
+                    if (i == s1.length() && j == s2.length()) break;
+                    if (!A[i][j]) continue;
+                    int c = s3.charAt(i + j);
+                    A[i+1][j] = (i < s1.length() && s1.charAt(i) == c);
+                    A[i][j+1] = (j < s2.length() && s2.charAt(j) == c);
+                }
+            }
+            return A[s1.length()][s2.length()];
+        }
+        
+    }
+    
+    
     public static void main(String[] args) {
         
-        String s1 = "ab";
-        String s2 = "ac";
-        String s3 = "aabc";
-        // "aadbbbaccc"
+        String s1 = "aacaac";
+        String s2 = "aacaaeaac";
+        String s3 = "aacaaeaaeaacaac";
         
-        Out.p(new Solution().isInterleave(s1, s2, s3));
+        Out.p(new Solution2().isInterleave(s1, s2, s3));
     }
     
 }

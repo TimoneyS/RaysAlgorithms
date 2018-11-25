fdcf669f725286890d89ce5ed96138bb2b73485d
Ray <Rays1990@163.com>
2018-11-25 13:27:24 +0800

    add

--- src/com/ray/LintCode/L_0032_Minimum_Window_Substring.java
+++ src/com/ray/LintCode/L_0032_Minimum_Window_Substring.java
@@ -15,6 +15,21 @@ import com.ray.io.Out;
  */
 public class L_0032_Minimum_Window_Substring {
 
+    /**
+     * 维护两个数组
+     * 
+     * cc 当前字符计数
+     * ct 目标字符计数
+     * 
+     * 寻找一个子字符串，其计数结果 cc，对于每一个字符 c 均有 cc[c] > ct[c]
+     * 目标是 寻找符合以上条件的最短的子字符串
+     * 
+     * 
+     * 
+     * 
+     * @author rays1
+     *
+     */
     static class Solution {
         /**
          * @param source
@@ -31,53 +46,48 @@ public class L_0032_Minimum_Window_Substring {
             
             int[] cc = new int[255]; // count current
             int[] ct = new int[255]; // count target
-            int[] leftOf = new int[source.length()];
+            int[] rightOf = new int[source.length()];
             
             int head = 0, tail = -1;
-            int cursor = 0;
+            int prev = 0;
             int remain = target.length(); // how many character remain that not in window 
             
             for (char c : at) ct[c] ++;
             
             for (int i = 0; i < as.length; i++) {
-                char c = as[i];
-                if (ct[c] > 0) {
-
-                    if (remain == target.length()) head = i;
-                    
-                    if (cc[c] < ct[c]) {
-                        if (remain >= 0) {
-                            tail = i;
-                            remain --;
-                        }
-                    }                    
-                    
-                    cc[c] += 1;
-                    leftOf[cursor] = i;
-                    cursor = i;
+                
+                if (ct[as[i]] <= 0) continue;
+                
+                if (remain == target.length()) head = i;
+                
+                if (cc[as[i]] < ct[as[i]] && remain >= 0) {
+                    tail = i;
+                    remain --;
+                }                    
+                
+                cc[as[i]] ++;
+                rightOf[prev] = i;
+                prev = i;
+                
+                // current char equals head char will strike a clean operation
+                if (cc[as[i]] > ct[as[i]] && as[head] == as[i]) {
                     
                     int j = head;
                     while (j < i) {
                         if (cc[as[j]] <= ct[as[j]]) break;
                         cc[source.charAt(j)] --;
-                        j = leftOf[j];
+                        j = rightOf[j];
                     }
                     
                     if (i-j <= tail-head) {
                         tail = i;
                         head = j;
                     } else {
-                        for (int k = head; k < j; k = leftOf[k]) cc[as[k]] ++;
+                        for (int k = head; k < j; k = rightOf[k]) cc[as[k]] ++;
                     }
-                    
-                    Out.sep();
-                    for (int k = 0; k < cc.length; k++) {
-                        if (cc[k] > 0) Out.pf("count[%s]=%s\n", (char)k, cc[k]);
-                    }
-                    
-                    Out.pf("source[%s]=%s,head=%s,tail=%s\n", i, c, head, tail);
                     
                 }
+                
             }
             
             return source.substring(head, tail+1);
@@ -86,8 +96,8 @@ public class L_0032_Minimum_Window_Substring {
     
     public static void main(String[] args) {
         
-        String source = "aaaaaaaaaaaabbbbbcdd";
-        String target = "abcdd";
+        String source = "aaaaaaaaaaaabbbbbbbbaccccccccccccccccabc";
+        String target = "abc";
         
         Out.p(new Solution().minWindow(source, target));
         

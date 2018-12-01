5fb3fdcfe2ce525bb1ba5e2fc06c950446a8ecc0
Ray <Rays1990@163.com>
2018-12-02 01:14:55 +0800

    ADD

--- src/com/ray/LintCode/L_0057_3Sum.java
+++ src/com/ray/LintCode/L_0057_3Sum.java
@@ -31,15 +31,56 @@ public class L_0057_3Sum {
          */
         public List<List<Integer>> threeSum(int[] numbers) {
             
+            List<List<Integer>>   rs = new ArrayList<List<Integer>>();
+            Map<Integer, Boolean> marked = new HashMap<Integer, Boolean>();
+            
+            for (int i = 0; i < numbers.length; i++) {
+                if (marked.containsKey(numbers[i])) continue;
+                
+                rs.addAll(twoSum(numbers, i+1, 0-numbers[i], marked));
+                
+                marked.put(numbers[i], true);
+            }
          
-            return null;
+            return rs;
+        }
+        
+        private List<List<Integer>> twoSum(int[] numbers, int start, int target, Map<Integer, Boolean> basemarked) {
+            
+            List<List<Integer>>   rs = new ArrayList<List<Integer>>();
+            Map<Integer, Boolean> marked = new HashMap<Integer, Boolean>();
+            Map<Integer, Integer> remain = new HashMap<>();
+            
+            
+            for (int i = start; i < numbers.length; i++) {
+                int num = numbers[i];
+                
+                if (marked.containsKey(num) || basemarked.containsKey(num)) continue;
+                if (remain.containsKey(num)) {
+                    int j = remain.get(num);
+                    List<Integer> l = new ArrayList<Integer>(3);
+                    l.add(numbers[start-1]);
+                    l.add(numbers[i]);
+                    l.add(numbers[j]);
+                    Collections.sort(l);
+                    marked.put(numbers[i], true);
+                    marked.put(numbers[j], true);
+                    
+                    rs.add(l);
+                }
+                
+                remain.put(target-num, i);
+                
+            }
+            
+            return rs;
         }
         
     }
     
     public static void main(String[] args) {
         
-        int[] numbers = {1,0,-1,-1,-1,-1,0,1,1,1};
+        int[] numbers = {-2,-3,-4,-5,-100,99,1,4,4,4,5,1,0,-1,2,3,4,5};
         
         Out.p(new Solution().threeSum(numbers));
         

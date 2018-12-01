88607977eae380341f516bdd937e5364da716d2e
Ray <Rays1990@163.com>
2018-12-01 16:52:24 +0800

    ADD

--- src/com/ray/LintCode/L_0054_String_to_Integer_atoi.java
+++ src/com/ray/LintCode/L_0054_String_to_Integer_atoi.java
@@ -1,5 +1,7 @@
 package com.ray.LintCode;
 
+import com.ray.io.Out;
+
 /**
  * Implement function atoi to convert a string to an integer.
  * 
@@ -25,8 +27,69 @@ public class L_0054_String_to_Integer_atoi {
          * @return: An integer
          */
         public int atoi(String str) {
-            return 0;
+            
+            int rs = 0;
+            boolean is_limit = false;
+            boolean is_neg = false;
+            int i = 0;
+            
+            while (i < str.length()) {
+                char c = str.charAt(i);
+                if (c == '+') {
+                    i++;
+                    break;
+                } else if (c == '-') {
+                    i++;
+                    is_neg = true;
+                    break;
+                } else if (c > 47 && c < 59) {
+                    break;
+                } else if (c == ' ') {
+                    i++;
+                } else {
+                    break;
+                }
+            }
+            
+            while (i < str.length()) {
+                char c = str.charAt(i);
+                i++;
+                
+                if (c < 48 || c > 58) {break;}
+                
+                if (is_limit) continue;
+                
+                if (is_neg) {
+                    int t = rs * 10 - c + 48;
+                    if (t > 0) {
+                        rs = Integer.MIN_VALUE;
+                        is_limit = true;
+                    } else {
+                        rs = t;
+                    }
+                } else {
+                    int t = rs * 10+(c-48);
+                    if (t < 0) {
+                        rs = Integer.MAX_VALUE;
+                        is_limit = true;
+                    } else {
+                        rs = t;
+                    }
+                }
+                
+            }
+            return rs;
         }
+    }
+    
+    public static void main(String[] args) {
+        
+        String str = " 2147483648";
+        
+        int rs = new Solution().atoi(str);
+        
+        Out.p(rs);
+        
     }
 
 }

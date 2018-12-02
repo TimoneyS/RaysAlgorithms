b8aa14a371378c524c4c99bcfef84e7fd59f65f9
Ray <Rays1990@163.com>
2018-12-03 01:21:08 +0800

    ADD

---
+++ src/com/ray/LintCode/L_0066_Binary_Tree_Preorder_Traversal.java
@@ -0,0 +1,68 @@
+package com.ray.LintCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import com.ray.io.Out;
+
+/**
+ * Given a binary tree, return the preorder traversal of its nodes' values.
+ *
+ * 前序遍历
+ *
+ * @author rays1
+ *
+ */
+public class L_0066_Binary_Tree_Preorder_Traversal {
+
+    static class Solution {
+        /**
+         * @param root: A Tree
+         * @return: Preorder in ArrayList which contains node values.
+         */
+        public List<Integer> preorderTraversal(TreeNode root) {
+            
+            List<Integer> rs = new ArrayList<>();
+            Stack<TreeNode> stack =  new Stack<>();
+            
+            stack.push(root);
+            while(!stack.isEmpty()) {
+                TreeNode node = stack.pop();
+                if (node == null) continue;
+                
+                rs.add(node.val);
+                stack.push(node.right);
+                stack.push(node.left);
+                
+            }
+            
+            return rs;
+        }
+    }
+    
+    static class TreeNode {
+        public int val;
+        public TreeNode left, right;
+        public TreeNode(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        TreeNode root = new TreeNode(1);
+        
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        
+        Out.p(new Solution().preorderTraversal(root));
+        
+    }
+
+}

8c4cccd58c95500590a804240a366156b9bc9a4e
Ray <Rays1990@163.com>
2018-12-02 13:26:39 +0800

    Add

---
+++ src/com/ray/LintCode/L_0061_Search_for_a_Range.java
@@ -0,0 +1,15 @@
+package com.ray.LintCode;
+
+/**
+ * Given a sorted array of n integers, find the starting and ending position of a given target value.
+ * 
+ * If the target is not found in the array, return [-1, -1].
+ * 
+ * @author rays1
+ *
+ */
+public class L_0061_Search_for_a_Range {
+
+
+
+}

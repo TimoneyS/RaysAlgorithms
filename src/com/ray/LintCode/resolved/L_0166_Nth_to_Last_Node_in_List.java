98602587117c05575eeeddd02486f31807c4f062
Ray <Rays1990@163.com>
2018-12-27 23:08:49 +0800

    寻找倒数第n个结点

---
+++ src/com/ray/LintCode/resolved/L_0166_Nth_to_Last_Node_in_List.java
@@ -0,0 +1,47 @@
+package com.ray.LintCode.resolved;
+
+import com.ray.io.Out;
+
+/**
+ * 寻找倒数第n个结点
+ *
+ * @author rays1
+ *
+ */
+public class L_0166_Nth_to_Last_Node_in_List {
+
+    static class Solution {
+        public ListNode nthToLast(ListNode head, int n) {
+            
+            ListNode n1 = null, n2;
+            
+            int dist = 0;
+            n2 = head;
+            while (true) {
+                if (dist == n) {
+                    n1 = head;
+                    break;
+                }
+                n2 = n2.next;
+                dist++;
+            }
+            
+            while (n1 != null && n2 != null) {
+                n1 = n1.next;
+                n2 = n2.next;
+            }
+            
+            return n1;
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        ListNode head = ListNode.parse("{1}");
+        Solution sol = new Solution();
+        ListNode node = sol.nthToLast(head, 1);
+        
+        Out.p(node.val);
+    }
+
+}

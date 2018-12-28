db860329158ec02c202cf531dc5aa10da24daa72
Ray <Rays1990@163.com>
2018-12-28 14:27:40 +0800

    L_0176_Route_Between_Two_Nodes_in_Graph

---
+++ src/com/ray/LintCode/resolved/L_0176_Route_Between_Two_Nodes_in_Graph.java
@@ -0,0 +1,38 @@
+package com.ray.LintCode.resolved;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
+ *
+ * @author rays1
+ *
+ */
+public class L_0176_Route_Between_Two_Nodes_in_Graph {
+
+    static class Solution {
+        public boolean hasRoute(ArrayList<DirectedGraphNode> graph, DirectedGraphNode s, DirectedGraphNode t) {
+            
+            HashSet<Integer> marked = new HashSet<>();
+            
+            marked.add(s.label);
+            dfs(s, t, marked);
+            return marked.contains(t.label);
+        }
+        
+        public void dfs(DirectedGraphNode s, DirectedGraphNode t, HashSet<Integer> marked) {
+            for (DirectedGraphNode n : s.neighbors) {
+                if (!marked.contains(n.label)) {
+                    marked.add(n.label);
+                    dfs(n, t, marked);
+                    if (marked.contains(t.label))
+                        break;
+                }
+            }
+            
+        }
+        
+    }
+
+}

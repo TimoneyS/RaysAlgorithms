92942fc5416a706016c35a24529e51a8818afa29
Ray <Rays1990@163.com>
2019-01-06 18:37:20 +0800

    L_0202_Segment_Tree_Query

---
+++ src/com/ray/LintCode/resolved/L_0202_Segment_Tree_Query.java
@@ -0,0 +1,44 @@
+package com.ray.LintCode.resolved;
+
+/**
+ * For an integer array (index from 0 to n-1, where n is the size of this array), in the corresponding SegmentTree, each node stores an extra attribute max to denote the maximum number in the interval of the array (index from start to end).
+ *
+ * Design a query method with three parameters root, start and end, find the maximum number in the interval [start, end] by the given root of segment tree.
+ *
+ * @author rays1
+ *
+ */
+public class L_0202_Segment_Tree_Query {
+
+    /**
+     * 首先对区间取交集，交集后如果区间和结点区间相等，则返回max，否则分别在左右结点中继续搜索
+     * @author rays1
+     *
+     */
+    static class Solution {
+        /**
+         * @param root: The root of segment tree.
+         * @param start: start value.
+         * @param end: end value.
+         * @return: The maximum number in the interval [start, end]
+         */
+        public int query(SegmentTreeNode root, int start, int end) {
+            
+            start   = Math.max(root.start, start);
+            end     = Math.min(root.end, end);
+            
+            if (start > end)
+                return 0;
+            
+            if (root.start == start && root.end == end) {
+                return root.max;
+            } else {
+                return Math.max(
+                        query(root.left, start, end),
+                        query(root.right, start, end));
+            }
+            
+        }
+    }
+    
+}
--- src/com/ray/LintCode/resolved/SegmentTreeNode.java
+++ src/com/ray/LintCode/resolved/SegmentTreeNode.java
@@ -1,11 +1,18 @@
 package com.ray.LintCode.resolved;
 
 public class SegmentTreeNode {
-    public int start, end;
+    public int start, end, max;
     public SegmentTreeNode left, right;
     public SegmentTreeNode(int start, int end) {
         this.start = start;
         this.end = end;
         this.left = this.right = null;
     }
+    public SegmentTreeNode(int start, int end, int max) {
+        this.start = start;
+        this.end = end;
+        this.max = max;
+        this.left = this.right = null;
+    }
+    
 }

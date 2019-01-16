c7d5d7c0e3f605b9bca218cca75165e474e9ab08
Ray <Rays1990@163.com>
2019-01-17 00:18:29 +0800

    372. Delete Node in a Linked List

---
+++ src/com/ray/LintCode/resolved/L_0372_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,23 @@
+package com.ray.LintCode.resolved;
+
+/**
+ * 继承方法删除单链表中的结点
+ *
+ * @author rays1
+ *
+ */
+public class L_0372_Delete_Node_in_a_Linked_List {
+
+    /**
+     * 用下个结点的值，覆盖当前结点，然后删除下个结点
+     * @author rays1
+     *
+     */
+    static class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+
+}

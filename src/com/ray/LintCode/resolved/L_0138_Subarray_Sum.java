50772987f2ada1428652a1bbe29e88aa52dc90f0
Ray <Rays1990@163.com>
2019-01-21 00:14:23 +0800

    L_0405_Submatrix_Sum

--- src/com/ray/LintCode/resolved/L_0138_Subarray_Sum.java
+++ src/com/ray/LintCode/resolved/L_0138_Subarray_Sum.java
@@ -1,25 +1,31 @@
 package com.ray.LintCode.resolved;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import com.ray.io.Out;
 
 /**
- * Given an integer array, find a subarray where the sum of numbers is zero.
- * Your code should return the index of the first number and the index of the last number.
+ * 给定一个整型数组，寻找和是0的子数组。返回子数组开始和结束索引。
  *
- * 给定一个整型数组，寻找和是0的子数组。返回第一个和最后一个数组的索引。
+ * 如 [ -3, 1, 2, -3, 4 ] 返回 [ 0, 2] 或 [ 1, 3]
  *
  * @author rays1
  *
  */
 public class L_0138_Subarray_Sum {
-
+    
+    /*
+     * 如果数组中从 i 到 j 部分的和为 0
+     * 那么必然数组之和 s[0..i-1] = s[0..j+1]
+     * 因此只要一直加，同时保存每次累加的结果
+     * 直到找到两个和相等的部分即可
+     */
     static class Solution {
         public List<Integer> subarraySum(int[] nums) {
+            
+            ArrayList<Integer> rs = new ArrayList<>();
             HashMap<Integer, Integer> map = new HashMap<>();
             map.put(0, -1);
             
@@ -27,16 +33,17 @@ public class L_0138_Subarray_Sum {
             for (int i = 0; i < nums.length; i++) {
                 sum += nums[i];
                 if (map.containsKey(sum)) {
-                    return Arrays.asList(new Integer[] {map.get(sum)+1, i});
+                    rs.add(map.get(sum)+1);
+                    rs.add(i);
+                    break;
                 }
                 map.put(sum, i);
             }
             
-            return new ArrayList<>();
+            return rs;
         }
         
     }
-    
     
     public static void main(String[] args) {
         

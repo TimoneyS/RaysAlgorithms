89274dcbffa499f19bc152054794499890ecb904
Ray <Rays1990@163.com>
2019-01-24 16:26:54 +0800

    L_0434_Number_of_Islands_II

--- src/com/rays/algo/search/UnionFind.java
+++ src/com/rays/algo/search/UnionFind.java
@@ -1,5 +1,7 @@
 package com.rays.algo.search;
 
+import com.ray.io.Out;
+
 /**
  * 
  * @author rays1
@@ -9,9 +11,11 @@ public class UnionFind {
     
     private int[] parent;  // parent[i] = parent of i
     private int count;     // number of components
+    
+    public int count() { return count; }
+    public boolean isConnected(int p, int q) { return find(p) == find(q); }
 
     public UnionFind(int n) {
-        if (n < 0) throw new IllegalArgumentException();
         count = n;
         parent = new int[n];
         for (int i = 0; i < n; i++) {
@@ -21,29 +25,35 @@ public class UnionFind {
 
     public int find(int p) {
         while (p != parent[p]) {
-            parent[p] = parent[parent[p]];    // path compression by halving
+            parent[p] = parent[parent[p]];    // 压缩路径
             p = parent[p];
         }
         return p;
-    }
-
-
-    public int count() {
-        return count;
-    }
-  
-    public boolean connected(int p, int q) {
-        return find(p) == find(q);
     }
   
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
         if (rootP == rootQ) return;
-
-        // make root of smaller rank point to root of larger rank
+        // 合并两个分支
         parent[rootQ] = rootP;
         count--;
+    }
+    
+    public static void main(String[] args) {
+        
+        UnionFind uf = new UnionFind(5);
+        
+        uf.union(1, 2);
+        Out.p(uf.parent);
+        uf.union(3, 4);
+        Out.p(uf.parent);
+        uf.union(1, 4);
+        Out.p(uf.parent);
+        
+        uf.find(4);
+        Out.p(uf.parent);
+        
     }
     
 }

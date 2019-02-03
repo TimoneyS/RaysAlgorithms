e0f48043fc61434bff85d3fa6147a3074bdc22fc
Ray <Rays1990@163.com>
2019-02-03 23:29:47 +0800

    L_0535_House_Robber_III

--- src/com/ray/LintCode/resolved/L_0392_House_Robber.java
+++ src/com/ray/LintCode/resolved/L_0392_House_Robber.java
@@ -15,15 +15,25 @@ import com.ray.io.Out;
 public class L_0392_House_Robber {
 
     /**
-     * 每次处理房屋 k 时，之前的访问记录了三个数字
-     * 抢劫了 k - 1 的最大收获
-     * 抢劫了 k - 2 的最大收获
-     * 抢劫了 k - 3 的最大收获
+     * 用 S[i] 表示从 0 - i 并且抢劫了房屋 i 能得到的最大收获。A[i] 表示房屋的储蓄
      * 
-     * 抢劫 k-1 的数据供下一个房屋使用，当前房屋的最大收获为
-     * max( mk-2, mk-3 ) + a[k]
+     * 对于选择抢劫第 i 栋房屋的情况，i-1 房屋固定不能被抢劫，而抢劫 i-2 和 i-3 栋房屋不能同时发生
+     *      可以抢劫第 i-2 栋房屋 S[i] = S[i-2] + A[i]
+     *      可以抢劫第 i-3 栋房屋 S[i] = S[i-3] + A[i]
+     *      抢劫 i-4 栋房屋和抢劫 i-2 栋房屋可以同时发生，因此不需要讨论。
+     * 因此
+     *      S[i] = max { S[i-2], S[i-3] } + A[i]
+     * -------------------------------------------------------------------------------------
+     * 进一步考虑
+     * 如果知道抢劫第 i-1 栋房屋的最大收获为 S[i-1]，假设用i替换i-1房屋，那么最大收获就是
+     *      S[i] = S[i-1] - A[i-1] + A[i]
+     * 替换了之后 i-1(替换后的i) 和 i-2 又不能同时发生，因此也要考虑未替换前的情况
+     *      S[i] = S[i-2] + A[i]
      * 
-     * 因为抢劫 房屋k时，抢劫 k-2 k 或者 k-3 k 两种选择（k-4 的情况 和 k-2 的情况可以同时发生，再之前的房屋同理）
+     * 因此
+     *      S[i] = max {S[i-1] - A[i-1], S[i-2]} + A[i]
+     * 
+     * 这样只需要保存前两次的结果即可。
      * 
      * @author rays1
      *
@@ -31,21 +41,46 @@ public class L_0392_House_Robber {
     static class Solution {
     
         public long houseRobber(int[] A) {
-            long[] mem = new long[3];
-            int i = 0;
-            for (int a : A) {
-                int j = (i + 1) % 3;
-                mem[i] = Math.max(mem[i], mem[j]) + a;
-                i = j;
+            long[] mem = new long[2];
+            int cur = 1, pre = 0;
+            
+            if(A.length > 0) mem[0] = A[0];
+            for (int i = 1; i < A.length; i++) {
+                pre = (cur + 1) % 2;
+                mem[cur] = Math.max(mem[cur], mem[pre]- i > 0 ? 0 : A[i-1]) + A[i];
+                cur = pre;
             }
-            return Math.max(mem[0],Math.max(mem[1], mem[2]));
+            
+            return Math.max(mem[0], mem[1]);
+        }
+        
+    }
+    
+    /**
+     * 用 
+     *      S[i] 表示到房屋 i 为止，抢劫 i 的最大收获
+     *      W[i] 表示到房屋 i 为止，不抢劫 i 的最大收获
+     * 则
+     *      S[i] = W[i-1] + A[i]
+     *      W[i] = max { S[i-1], W[i-1] }
+     */
+    class Solution1 {
+        
+        public long houseRobber(int[] A) {
+            long s = 0, w = 0, pres = 0;
+            for (int i = 0; i < A.length; i++) {
+                pres = s;
+                s = w + A[i];
+                w = Math.max(w, pres);
+            }
+            return Math.max(s, w);
         }
         
     }
     
     public static void main(String[] args) {
         
-        int[] A = {3, 8, 4};
+        int[] A = {3,2,1,5};
         
         Out.p(new Solution().houseRobber(A));
         

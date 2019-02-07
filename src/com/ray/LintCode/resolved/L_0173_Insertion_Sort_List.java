78b00b15aefcd3cbb0c5a8fed5e6895e40b5ae6e
Ray <Rays1990@163.com>
2019-02-08 00:01:23 +0800

    L_0599_Insert_into_a_Cyclic_Sorted_List

--- src/com/ray/LintCode/resolved/L_0173_Insertion_Sort_List.java
+++ src/com/ray/LintCode/resolved/L_0173_Insertion_Sort_List.java
@@ -3,34 +3,46 @@ package com.ray.LintCode.resolved;
 import com.ray.LintCode.util.ListNode;
 
 /**
- * 插入排序，排序链表
+ * 用插入排序的思想，排序链表
  *
  * @author rays1
  *
  */
 public class L_0173_Insertion_Sort_List {
 
+    /**
+     * 插入排序将链表分为两部分，已排序和未排序部分
+     * 
+     * 插入排序每次从未排序部分寻找最小的结点，插入已经排序的部分的末尾
+     * 
+     * @author rays1
+     *
+     */
     static class Solution {
         public ListNode insertionSortList(ListNode head) {
+            
             ListNode pile = new ListNode(Integer.MIN_VALUE);
-            pile.next = head;
+            pile.next     = head;
             
-            ListNode sortedTail = pile;
-            
-            while (sortedTail.next != null) {
-                // 从 sortedTail 之后寻找最小元素
-                ListNode beforMin = sortedTail;
-                for (ListNode n = sortedTail; n.next != null; n = n.next) {
-                    if (n.next.val < beforMin.next.val) {
-                        beforMin = n;
+            ListNode lastSorted = pile;
+            while (lastSorted.next != null) {
+                
+                // 从 lastSorted 之后寻找最小结点的前置结点
+                ListNode prev = lastSorted;
+                for (ListNode n = lastSorted; n.next != null; n = n.next) {
+                    if (n.next.val < prev.next.val) {
+                        prev = n;
                     }
                 }
                 
-                ListNode min = beforMin.next;
-                beforMin.next        = beforMin.next.next;
-                min.next = sortedTail.next;
-                sortedTail.next = min;
-                sortedTail      = sortedTail.next;
+                // 将min结点从原位置移除
+                ListNode min    = prev.next;
+                prev.next       = prev.next.next;
+                // min结点插入已经排序的末尾
+                min.next        = lastSorted.next;
+                lastSorted.next = min;
+                
+                lastSorted      = lastSorted.next;
             }
             
             return pile.next;
---
+++ src/com/ray/LintCode/resolved/L_0219_Insert_Node_in_Sorted_Linked_List.java
@@ -0,0 +1,50 @@
+package com.ray.LintCode.resolved;
+
+import com.ray.LintCode.util.ListNode;
+import com.ray.io.Out;
+
+/**
+ * 向有序的链表中插入新结点
+ *
+ * @author rays1
+ *
+ */
+public class L_0219_Insert_Node_in_Sorted_Linked_List {
+
+    /**
+     * 增加一个桩，寻找第一个下一个结点大于val的结点。
+     * @author rays1
+     *
+     */
+    static class Solution {
+    
+        public ListNode insertNode(ListNode head, int val) {
+            ListNode pile = new ListNode(Integer.MIN_VALUE);
+            pile.next = head;
+            
+            ListNode node = pile;
+            while (node != null && node.next != null) {
+                if (node.next.val < val) node = node.next;
+                else break;
+            }
+            
+            ListNode t = new ListNode(val);
+            t.next = node.next;
+            node.next = t;
+            
+            return pile.next;
+        }
+    
+    }
+    
+    public static void main(String[] args) {
+        
+        ListNode head = ListNode.parse("{1,2,3,4}");
+        int val = 5;
+        
+        Out.p(head);
+        Out.p(new Solution().insertNode(head, val));
+        
+    }
+
+}

37871aa4e10075d0b70ae5cfa04322d4f7f95e71
Ray <Rays1990@163.com>
2019-03-01 12:31:57 +0800

    L_0633_Find_the_Duplicate_Number

--- src/com/ray/LintCode/resolved/L_0102_Linked_List_Cycle.java
+++ src/com/ray/LintCode/resolved/L_0102_Linked_List_Cycle.java
@@ -4,7 +4,7 @@ import com.ray.LintCode.util.ListNode;
 import com.ray.io.Out;
 
 /**
- * Given a linked list, determine if it has a cycle in it.
+ * 给定一个链表，检测是否有环
  *
  * @author rays1
  *
@@ -13,17 +13,13 @@ public class L_0102_Linked_List_Cycle {
 
     static class Solution {
         
-        /**
-         * @param head: The first node of linked list.
-         * @return: True if it has a cycle, or false
-         */
         public boolean hasCycle(ListNode head) {
-            ListNode n1 = head, n2 = head;
+            ListNode slow = head, fast = head;
             while (true) {
-                if (n1 == null || n2 == null || n2.next == null) return false;
-                n1 = n1.next;
-                n2 = n2.next.next;
-                if (n1 == n2) return true;
+                if (slow == null || fast == null || fast.next == null) return false;
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) return true;
             }
         }
         
--- src/com/ray/LintCode/resolved/L_0103_Linked_List_Cycle_II.java
+++ src/com/ray/LintCode/resolved/L_0103_Linked_List_Cycle_II.java
@@ -4,8 +4,6 @@ import com.ray.LintCode.util.ListNode;
 import com.ray.io.Out;
 
 /**
- * Given a linked list, return the node where the cycle begins.
- *
  * 检测链表中的环，若有环，返回环的起始顶点
  * 否则返回 null
  *
@@ -15,50 +13,50 @@ import com.ray.io.Out;
 public class L_0103_Linked_List_Cycle_II {
 
     /**
-     * xxxxxxxxxxxx yyyyyyyy
-     * 
-     * xxxxxxxxxxxx yyyyyyyy
-     *              yyyyyyyyyyyyyyy
-     *              yyyyyyyyyyyyyyy
-     *              
-     * 假设 非环部分长度为 x 环的部分长度为  y
-     * 
-     * 则慢遍历长度   ： x + y1
-     *   快遍历长度   ： x + y2
-     * 快遍历长度为慢遍历长度的 2 倍
+     * 假设
+     *      非环部分长度为 x
+     *      环的部分长度为 y
+     * 则
+     *      慢指针遍历长度：x + y1
+     *      快指针遍历长度：x + y2
+     * 快指针遍历长度为慢指针遍历长度的 2 倍，则有
      *      2*(x+y1) = x + y2
      *      x + 2*y1 = y2
      * 可得 
      *      y2 - y1 = x + y1
-     * 同时在环的部分，y2 - y1 = n * y
+     * 同时快指针和慢指针相遇一定时因为快指针沿着环夺走了几圈，因此
+     *      x + y2 - x + y1 = y2 - y1 = n * y
      * 则可得
      *      x + y1 = n * y
-     *      x = n * y - y1
-     * 表示从x开始遍历到环的起点，和从慢遍历的终点到环的起点的距离是一样的（或者满遍历多绕几圈）
+     *      x = n*y - y1
+     *      
+     * 假设 z 为慢遍历的终点到环的起点的距离，则
+     *      z+y1 = k1*y
+     *      y1 = k1*y - z
+     *      x = n*y - k1*y + z
+     *      x = k*y + z
+     *      
+     * 表示从起点到环的入口，和从慢遍历的终点到环的入口的距离相差 k 个环的距离
+     * 那么从起点开始和从慢遍历的终点开始，依次前进，其相遇的位置即为环的入口
      * 
      * @author rays1
      *
      */
     static class Solution {
         
-        /**
-         * @param head: The first node of linked list.
-         * @return: The node where the cycle begins. if there is no cycle, return null
-         */
         public ListNode detectCycle(ListNode head) {
             if (head == null) return null;
             
-            ListNode n1 = head, n2 = head.next;
-            while (n1 != n2) {
-                if (n2 == null || n2.next == null)
-                    return null;
-                n1 = n1.next;
-                n2 = n2.next.next;
+            ListNode slow = head, fast = head.next;
+            while (slow != fast) {
+                if (fast == null || fast.next == null) return null;
+                slow = slow.next;
+                fast = fast.next.next;
             }
-            Out.p(n1.val);
-            while (head != n1.next) {
+            
+            while (head != slow.next) {
                 head = head.next;
-                n1 = n1.next;
+                slow = slow.next;
             }
             return head;
         }

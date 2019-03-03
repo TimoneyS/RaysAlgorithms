e1103e1d47ca9f67853adfe3d36502459f74b908
Ray <Rays1990@163.com>
2019-03-03 22:37:49 +0800

    L_0650_Find_Leaves_of_Binary_Tree

---
+++ src/com/ray/LintCode/resolved/L_0650_Find_Leaves_of_Binary_Tree.java
@@ -0,0 +1,67 @@
+package com.ray.LintCode.resolved;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.ray.LintCode.util.TreeNode;
+import com.ray.io.Out;
+
+/**
+ * 给定一个二叉树，不断的摘下所有的叶子，直到树为空。
+ * 
+ * 例如：
+ * 
+ *  输入：{1,2,3,4,5}
+ *  输出：[[4, 5, 3], [2], [1]].
+ *  
+ *       1            1         
+ *      / \          / \ 
+ *     2   3   -->  2   3  --> 1
+ *    / \      
+ *   4   5     
+ *
+ * @author rays1
+ *
+ */
+public class L_0650_Find_Leaves_of_Binary_Tree {
+
+    /**
+     * 后序遍历数组，记录每个结点离叶子节点的最远距离
+     * 这个距离就是结点被删除的次序，保存到map中。
+     * 
+     * 遍历结束后，按照map中存储的距离信息，拼出结果数组。
+     * 
+     * @author rays1
+     *
+     */
+    static class Solution {
+    
+        public List<List<Integer>> findLeaves(TreeNode root) {
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            List<List<Integer>> rs = new ArrayList<>();
+            walk(root, map);
+            for (int i = 0; i < map.size(); i++)
+                rs.add(map.get(i));
+            return rs;
+        }
+        
+        int walk(TreeNode root, Map<Integer, List<Integer>> map) {
+            if (root == null) return -1;
+            int d = Math.max(walk(root.left, map), walk(root.right, map))+1;
+            map.putIfAbsent(d, new ArrayList<>());
+            map.get(d).add(root.val);
+            return d;
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        TreeNode root = TreeNode.parse("{1,2,3,4,5}");
+        Out.p(new Solution().findLeaves(root));
+        
+    }
+
+}

c175c660963615866c686b1322ccbf7f239112d1
Ray <Rays1990@163.com>
2019-03-31 12:25:22 +0800

    update

--- src/com/ray/LintCode/resolved/L_0003_Digit_Counts.java
+++ src/com/ray/LintCode/resolved/L_0003_Digit_Counts.java
@@ -2,7 +2,7 @@ package com.ray.LintCode.resolved;
 
 /**
  * Count the number of k's between 0 and n. k can be 0 - 9.
- * <p>
+ * 
  * if n = 12, k = 1 in
  * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
  * we have FIVE 1's (1, 10, 11, 12)
@@ -12,13 +12,8 @@ package com.ray.LintCode.resolved;
 public class L_0003_Digit_Counts {
     
     static class Solution {
-        /**
-         * @param k: An integer
-         * @param n: An integer
-         * @return: An integer denote the count of digit k in 1..n
-         */
+        
         public int digitCounts(int k, int n) {
-            // write your code here
             int sum = 0;
             for (int i = 0; i <= n;  i ++)
                 sum += count(i, k);

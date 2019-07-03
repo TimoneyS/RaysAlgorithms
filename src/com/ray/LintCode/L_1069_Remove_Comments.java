557220587a557e1586d43ced9cfee374c7107830
Ray <Rays1990@163.com>
2019-07-03 17:53:15 +0800

    add

--- src/com/ray/LintCode/L_1069_Remove_Comments.java
+++ src/com/ray/LintCode/L_1069_Remove_Comments.java
@@ -10,15 +10,15 @@ import com.ray.io.Out;
  *   
  *   The string `//` denotes a line comment, which represents that it and rest of the characters to the right of it in the same line should be ignored.
  *   
- *   The string `/*` denotes a block comment, which represents that all characters until the next (non-overlapping) occurrence of `*/ `should be ignored. (Here, occurrences happen in reading order: line by line from left to right.) To be clear, the string `/*/` does not yet end the block comment, as the ending would be overlapping the beginning.
+ *   The string `/*` denotes a block comment, which represents that all characters until the next (non-overlapping) occurrence of ` `should be ignored. (Here, occurrences happen in reading order: line by line from left to right.) To be clear, the string `/` does not yet end the block comment, as the ending would be overlapping the beginning.
  *   
  *   The first effective comment takes precedence over others: if the string` //` occurs in a block comment, it is ignored. Similarly, if the string `/*` occurs in a line or block comment, it is also ignored.
  *   
  *   If a certain line of code is empty after removing comments, you must not output that line: each string in the answer list will be non-empty.
  *   
- *   There will be no control characters, single quote, or double quote characters. For example, `source = "string s = "/* Not a comment. */";"` will not be a test case. (Also, nothing else such as defines or macros will interfere with the comments.)
+ *   There will be no control characters, single quote, or double quote characters. For example, `source = "string s = "/* Not a comment. *";"` will not be a test case. (Also, nothing else such as defines or macros will interfere with the comments.)
  *   
- *   It is guaranteed that every open block comment will eventually be closed, so` /*` outside of a line or block comment always starts a new comment.
+ *   It is guaranteed that every open block comment will eventually be closed, so` *` outside of a line or block comment always starts a new comment.
  *   
  *   Finally, implicit newline characters can be deleted by block comments. Please see the examples below for details.
  *   
@@ -28,21 +28,21 @@ import com.ray.io.Out;
  *   ```
  *   Example 1:
  *   	Input: 
- *   	source = ["/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"]
+ *   	source = ["/*Test program *", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing *", "a = b + c;", "}"]
  *   	
  *   	Output: ["int main()","{ ","  ","int a, b, c;","a = b + c;","}"]
  *   	
  *   	Explanation:
  *   	The line by line code is visualized as below:
- *   	/*Test program */
+ *   	Test program 
  *   	int main()
  *   	{ 
  *   		// variable declaration 
  *   		int a, b, c;
- *   	/* This is a test
+ *   	This is a test
  *   		 multiline  
  *   		 comment for 
- *   		 testing */
+ *   		 testing
  *   		a = b + c;
  *   	}
  *   
@@ -55,7 +55,7 @@ import com.ray.io.Out;
  *   
  *   Example 2:
  *   	Input: 
- *   	source = ["a/*comment", "line", "more_comment*/b"]
+ *   	source = ["a/*comment", "line", "more_comment*b"]
  *   
  *   	Output: ["ab"]
  *   ```

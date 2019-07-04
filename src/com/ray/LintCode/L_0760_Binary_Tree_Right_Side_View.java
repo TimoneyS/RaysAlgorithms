6e40feb157ee985d7d3ce596e0645e8ec161ba2f
Ray <Rays1990@163.com>
2019-07-04 16:21:36 +0800

    add

---
+++ src/com/ray/LintCode/L_0760_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,56 @@
+package com.ray.LintCode;
+
+import com.ray.io.Out;
+
+/**
+ * Description :
+ *   Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom
+ *
+ * Example :
+ *   **Example 1**
+
+```
+Input: {1,2,3,#,5,#,4}
+Output: [1,3,4]
+Explanation:
+   1            
+ /   \
+2     3         
+ \     \
+  5     4       
+```
+
+**Example 2**
+
+```plain
+Input: {1,2,3}
+Output: [1,3]
+Explanation:
+   1            
+ /   \
+2     3        
+```
+ *
+ * Challenge :
+ *   
+ *
+ * @author rays1
+ * @url    https://www.lintcode.cn/problem/binary-tree-right-side-view/description
+
+ * @date   2019-07-04 16:21:26
+ */
+public class L_0760_Binary_Tree_Right_Side_View {
+
+    static class Solution {
+    
+        
+    
+    }
+    
+    public static void main(String[] args) {
+        
+        Out.p(new Solution());
+        
+    }
+
+}

d1d6c570c24885437f3f7209e0ec11141854fee1
Ray <Rays1990@163.com>
2019-07-05 15:45:44 +0800

    add

--- src/com/ray/LintCode/resolved/L_0903_Range_Addition.java
+++ src/com/ray/LintCode/resolved/L_0903_Range_Addition.java
@@ -1,37 +1,37 @@
-package com.ray.LintCode;
+package com.ray.LintCode.resolved;
 
 import com.ray.io.Out;
 
 /**
  * Description :
  *   Assume you have an array of length `n` initialized with all `0`'s and are given `k` update operations.
-
-Each operation is represented as a triplet: `[startIndex, endIndex, inc]` which increments each element of subarray `A[startIndex ... endIndex]` (startIndex and endIndex inclusive) with `inc`.
-
-Return the modified array after all `k` operations were executed.
- *
+ *  
+ *   Each operation is represented as a triplet: `[startIndex, endIndex, inc]` which increments each element of subarray `A[startIndex ... endIndex]`
+ *    (startIndex and endIndex inclusive) with `inc`.
+ *  
+ *   Return the modified array after all `k` operations were executed.
+ *  
  * Example :
- *   ```
-Given:
-length = 5,
-updates = 
-[
-[1,  3,  2],
-[2,  4,  3],
-[0,  2, -2]
-]
-return [-2, 0, 3, 5, 3]
-
-Explanation:
-Initial state:
-[ 0, 0, 0, 0, 0 ]
-After applying operation [1, 3, 2]:
-[ 0, 2, 2, 2, 0 ]
-After applying operation [2, 4, 3]:
-[ 0, 2, 5, 5, 3 ]
-After applying operation [0, 2, -2]:
-[-2, 0, 3, 5, 3 ]
-```
+ *  Given:
+ *  length = 5,
+ *  updates = 
+ *   [
+ *     [1,  3,  2],
+ *     [2,  4,  3],
+ *     [0,  2, -2]
+ *   ]
+ *  return [-2, 0, 3, 5, 3]
+ *  
+ *  Explanation:
+ *  Initial state:
+ *    [ 0, 0, 0, 0, 0 ]
+ *  After applying operation [1, 3, 2]:
+ *    [ 0, 2, 2, 2, 0 ]
+ *  After applying operation [2, 4, 3]:
+ *    [ 0, 2, 5, 5, 3 ]
+ *  After applying operation [0, 2, -2]:
+ *    [-2, 0, 3, 5, 3 ]
+ *  ```
  *
  * Challenge :
  *   
@@ -44,7 +44,15 @@ public class L_0903_Range_Addition {
 
     static class Solution {
     
-        
+        public int[] getModifiedArray(int length, int[][] updates) {
+            int[] rs = new int[length];
+            for (int[] update : updates) {
+                for (int i = update[0]; i <= update[1]; i++) {
+                    rs[i] += update[2];
+                }
+            }
+            return rs;
+        }
     
     }
     

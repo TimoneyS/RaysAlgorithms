edf7808a01de41980b108f9374cc7ce79b011971
Ray <Rays1990@163.com>
2019-07-11 19:49:41 +0800

    ADD

--- src/com/ray/LintCode/done/L_0379_Reorder_array_to_construct_the_minimum_number.java
+++ src/com/ray/LintCode/done/L_0379_Reorder_array_to_construct_the_minimum_number.java
@@ -1,4 +1,4 @@
-package com.ray.LintCode.resolved;
+package com.ray.LintCode.done;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -6,25 +6,44 @@ import java.util.PriorityQueue;
 import com.ray.io.Out;
 
 /**
- * 通过重新排序给定的非负整数数组来构造最小数字。
- * 
- * 如数组 [3, 32, 321]， 有 6 种可能的排列再组成数字
- *  
- *  [3, 32, 321] = 332321 
- *  [3, 321, 32] = 332132
- *  [32, 3, 321] = 323321
- *  [32, 321, 3] = 323213
- *  [321, 3, 32] = 321332
- *  [321, 32, 3] = 321323
- *  
- *  其中最小排序方式为 [321, 32, 3]的数字为 321323 
- *  
- * @author rays1
+ * 描述：
+ *      Construct minimum number by reordering a given non-negative integer array. Arrange them such that they form the minimum number.
  *
+ * 用例：
+ *      **Example 1:**
+ *      ```
+ *      Input:[2,1]
+ *      Output:[1,2]
+ *       Explanation: there are 2 possible numbers can be constructed by reordering the array:
+ *                1+2=12
+ *                2+1=21
+ *      So after reordering, the minimum number is 12, and return it.
+ *      ```
+ *      **Example 2:**
+ *      ```
+ *      Input:[3, 32, 321]
+ *      Output:[321, 32, 3]
+ *       Explanation: there are 6 possible numbers can be constructed by reordering the array:
+ *      	3+32+321=332321
+ *      	3+321+32=332132
+ *      	32+3+321=323321
+ *      	32+321+3=323213
+ *      	321+3+32=321332
+ *      	321+32+3=321323
+ *      So after reordering, the minimum number is 321323, and return it.
+ *      ```
+ *
+ * 挑战：
+ *      Do it in O(nlogn) time complexity.
+ *
+ * 难度： Medium
+ *   
+ * @author rays1
+ * @url    https://www.lintcode.cn/problem/reorder-array-to-construct-the-minimum-number/description
+ * @date   2019-07-11 18:32:19
  */
 public class L_0379_Reorder_array_to_construct_the_minimum_number {
-    
-    
+
     /**
      * 数字按照以下逻辑排序
      *      3  和 321 比较则等同 333 和 321 比较
@@ -68,11 +87,10 @@ public class L_0379_Reorder_array_to_construct_the_minimum_number {
         }
     }
     
-   
-   public static void main(String[] args) {
-       
-       int[] nums = {0, 0, 0, 0};
-       
-       Out.p(new Solution().minNumber(nums));
-   }
+    public static void main(String[] args) {
+        
+        Out.p(new Solution());
+        
+    }
+
 }
--- src/com/ray/LintCode/done/L_0380_Intersection_of_Two_Linked_Lists.java
+++ src/com/ray/LintCode/done/L_0380_Intersection_of_Two_Linked_Lists.java
@@ -1,5 +1,6 @@
-package com.ray.LintCode;
+package com.ray.LintCode.done;
 
+import com.ray.LintCode.util.ListNode;
 import com.ray.io.Out;
 
 /**
@@ -42,10 +43,35 @@ import com.ray.io.Out;
  */
 public class L_0380_Intersection_of_Two_Linked_Lists {
 
+
+    /**
+     * 过河拆桥的解法，先用一个结点再遍历的过程中，将所有的节点都指向桩节点
+     * 第二个结点遍历时，发现的第一个指向桩的结点，就是交汇的结点。
+     * 如果没有发现此类结点，则说明无交汇结点。
+     * @author rays1
+     *
+     */
     static class Solution {
-    
-        
-    
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode rs = null;
+            
+            ListNode pile = new ListNode(Integer.MAX_VALUE);
+            ListNode prev = headA;
+            while ((prev = headA) != null) {
+                headA = headA.next;
+                prev.next = pile;
+            }
+            
+            while (headB != null && headB.next != null) {
+                if (headB.next.val == pile.val) {
+                    rs = headB;
+                    break;
+                }
+                headB = headB.next;
+            }
+            
+            return rs;
+        }
     }
     
     public static void main(String[] args) {

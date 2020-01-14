e490cf8b0d53ef27c0ddc1139d9ce76e88db9e9e
Ray <Rays1990@163.com>
2020-01-14 23:48:36 +0800

    LIS

--- src/com/ray/LintCode/temp/L_0076_Longest_Increasing_Subsequence.java
+++ src/com/ray/LintCode/temp/L_0076_Longest_Increasing_Subsequence.java
@@ -22,47 +22,27 @@ import com.ray.io.Out;
  * @since   2019-07-11 18:29:53
  */
 public class L_0076_Longest_Increasing_Subsequence {
-
     /**
-     * 用 top 记录到当前元素为止，能够生成的各种长度的序列中，最小的终点元素。
-     * 
-     * 如：数组  4,5,1,2,3
-     * 
-     * 第一次：[4],5,1,2,3
-     *    top :  4 0
-     *      
-     * 第二次：4,[5],1,2,3
-     *    top :  4 5
-     * 
-     * 第三次：4,5,[1],2,3
-     *    top :  1 5
-     *    
-     * 第四次：4,5,1,[2],3
-     *    top :  1 2
-     *    
-     * 第五次：4,5,1,2,[3]
-     *    top :  1 2 3
-     * 
-     * 算法复杂度 O(n log n)
-     *    
+     * top 记录当前能够生成的各种长度的序列的最小终点元素, top[0] 表示长度为 1 的序列中最小的终点元素
+     *
+     * 如果一个 num 比 top 中最大的元素top[x]大，那么说明 num 比最长的序列的最大元素还大
+     * 那么 num + 序列 x 构成的新序列将更长，那么可以令 top[x+1] = num
+     *
+     * 如果 num 位于 top 中的 top[i] 和 top[j] 之间，说明 num + 序列 i 构成的新序列比序列 j 更优（长度一样，终点更小）
+     * 则更新 top[j] = num
      */
     static class Solution {
-        
         public int longestIncreasingSubsequence(int[] nums) {
-            
             int len = 0;
             int[] top = new int[nums.length+1];
-            
             for (int num : nums) {
                 int idx = Arrays.binarySearch(top, 0, len, num);
                 if (idx < 0) idx = - idx - 1;
                 top[idx] = num;
                 if (idx == len) len ++;
             }
-            
             return len;
         }
-        
     }
     
     public static void main(String[] args) {
@@ -71,5 +51,4 @@ public class L_0076_Longest_Increasing_Subsequence {
         
         Out.p(new Solution().longestIncreasingSubsequence(nums));
     }
-
 }
